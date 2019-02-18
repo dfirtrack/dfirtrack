a595fb8c6c8b484f909e8def451f3145aab57ff5
Mathias Stuhlmacher <mathias.stuhlmacher@gmx.de>
2019-02-18 21:54:24 +0100

    added template testing for domains

--- dfirtrack_main/tests/test_domain_views.py
+++ dfirtrack_main/tests/test_domain_views.py
@@ -16,7 +16,7 @@ class DomainViewTestCase(TestCase):
         test_user = User.objects.create_user(username='testuser', password='jjSeshxL17aDEdqkt8tP')
         test_user.save()
 
-    def test_domains_list_response_not_logged_in(self):
+    def test_domains_list_not_logged_in(self):
 
         # create url
         destination = '/login/?next=' + urllib.parse.quote('/domains/', safe='')
@@ -25,7 +25,7 @@ class DomainViewTestCase(TestCase):
         # compare
         self.assertRedirects(response, destination, status_code=302, target_status_code=200)
 
-    def test_domains_list_response_logged_in(self):
+    def test_domains_list_logged_in(self):
 
         # login testuser
         login = self.client.login(username='testuser', password='jjSeshxL17aDEdqkt8tP')
@@ -33,6 +33,15 @@ class DomainViewTestCase(TestCase):
         response = self.client.get('/domains/')
         # compare
         self.assertEqual(response.status_code, 200)
+
+    def test_domains_list_template(self):
+
+        # login testuser
+        login = self.client.login(username='testuser', password='jjSeshxL17aDEdqkt8tP')
+        # get response
+        response = self.client.get('/domains/')
+        # compare
+        self.assertTemplateUsed(response, 'dfirtrack_main/domain/domains_list.html')
 
     def test_domains_list_get_user_context(self):
 
@@ -43,7 +52,7 @@ class DomainViewTestCase(TestCase):
         # compare
         self.assertEqual(str(response.context['user']), 'testuser')
 
-    def test_domains_detail_response_not_logged_in(self):
+    def test_domains_detail_not_logged_in(self):
 
         # get object
         domain_1 = Domain.objects.get(domain_name='domain_1')
@@ -54,7 +63,7 @@ class DomainViewTestCase(TestCase):
         # compare
         self.assertRedirects(response, destination, status_code=302, target_status_code=200)
 
-    def test_domains_detail_response_logged_in(self):
+    def test_domains_detail_logged_in(self):
 
         # get object
         domain_1 = Domain.objects.get(domain_name='domain_1')
@@ -64,6 +73,17 @@ class DomainViewTestCase(TestCase):
         response = self.client.get('/domains/' + str(domain_1.domain_id))
         # compare
         self.assertEqual(response.status_code, 200)
+
+    def test_domains_detail_template(self):
+
+        # get object
+        domain_1 = Domain.objects.get(domain_name='domain_1')
+        # login testuser
+        login = self.client.login(username='testuser', password='jjSeshxL17aDEdqkt8tP')
+        # get response
+        response = self.client.get('/domains/' + str(domain_1.domain_id))
+        # compare
+        self.assertTemplateUsed(response, 'dfirtrack_main/domain/domains_detail.html')
 
     def test_domains_detail_get_user_context(self):
 
@@ -76,7 +96,7 @@ class DomainViewTestCase(TestCase):
         # compare
         self.assertEqual(str(response.context['user']), 'testuser')
 
-    def test_domains_add_response_not_logged_in(self):
+    def test_domains_add_not_logged_in(self):
 
         # create url
         destination = '/login/?next=' + urllib.parse.quote('/domains/add', safe='')
@@ -85,7 +105,7 @@ class DomainViewTestCase(TestCase):
         # compare
         self.assertRedirects(response, destination, status_code=302, target_status_code=200)
 
-    def test_domains_add_response_logged_in(self):
+    def test_domains_add_logged_in(self):
 
         # login testuser
         login = self.client.login(username='testuser', password='jjSeshxL17aDEdqkt8tP')
@@ -93,6 +113,15 @@ class DomainViewTestCase(TestCase):
         response = self.client.get('/domains/add')
         # compare
         self.assertEqual(response.status_code, 200)
+
+    def test_domains_add_template(self):
+
+        # login testuser
+        login = self.client.login(username='testuser', password='jjSeshxL17aDEdqkt8tP')
+        # get response
+        response = self.client.get('/domains/add')
+        # compare
+        self.assertTemplateUsed(response, 'dfirtrack_main/domain/domains_add.html')
 
     def test_domains_add_get_user_context(self):
 
@@ -103,7 +132,7 @@ class DomainViewTestCase(TestCase):
         # compare
         self.assertEqual(str(response.context['user']), 'testuser')
 
-    def test_domains_edit_response_not_logged_in(self):
+    def test_domains_edit_not_logged_in(self):
 
         # get object
         domain_1 = Domain.objects.get(domain_name='domain_1')
@@ -114,7 +143,7 @@ class DomainViewTestCase(TestCase):
         # compare
         self.assertRedirects(response, destination, status_code=302, target_status_code=200)
 
-    def test_domains_edit_response_logged_in(self):
+    def test_domains_edit_logged_in(self):
 
         # get object
         domain_1 = Domain.objects.get(domain_name='domain_1')
@@ -124,6 +153,17 @@ class DomainViewTestCase(TestCase):
         response = self.client.get('/domains/' + str(domain_1.domain_id) + '/edit/')
         # compare
         self.assertEqual(response.status_code, 200)
+
+    def test_domains_edit_template(self):
+
+        # get object
+        domain_1 = Domain.objects.get(domain_name='domain_1')
+        # login testuser
+        login = self.client.login(username='testuser', password='jjSeshxL17aDEdqkt8tP')
+        # get response
+        response = self.client.get('/domains/' + str(domain_1.domain_id) + '/edit/')
+        # compare
+        self.assertTemplateUsed(response, 'dfirtrack_main/domain/domains_edit.html')
 
     def test_domains_edit_get_user_context(self):
 
@@ -136,7 +176,7 @@ class DomainViewTestCase(TestCase):
         # compare
         self.assertEqual(str(response.context['user']), 'testuser')
 
-#    def test_domains_detail_response_logged_in_not_existing(self):
+#    def test_domains_detail_logged_in_not_existing(self):
 #
 #        # login testuser
 #        login = self.client.login(username='testuser', password='jjSeshxL17aDEdqkt8tP')
