5c6c2579e5654369b6772d910412762d83ede3b6
Mathias Stuhlmacher <mathias.stuhlmacher@gmx.de>
2019-05-12 19:58:31 +0200

    added tests for tasks

---
+++ dfirtrack_main/tests/task/test_task_models.py
@@ -0,0 +1,240 @@
+from django.contrib.auth.models import User
+from django.test import TestCase
+from django.utils import timezone
+from dfirtrack_main.models import System, Systemstatus, Task, Taskname, Taskpriority, Taskstatus
+
+class TaskModelTestCase(TestCase):
+    """ task model tests """
+
+    @classmethod
+    def setUpTestData(cls):
+
+        # create user
+        test_user = User.objects.create_user(username='testuser_task', password='Zta9LblLVMWdYoXMTBZE')
+
+        # create object
+        systemstatus_1 = Systemstatus.objects.create(systemstatus_name='systemstatus_1')
+
+        # create object
+        system_1 = System.objects.create(
+            system_name = 'system_1',
+            systemstatus = systemstatus_1,
+            system_modify_time = timezone.now(),
+            system_created_by_user_id = test_user,
+            system_modified_by_user_id = test_user,
+        )
+
+        # create object
+        taskname_1 = Taskname.objects.create(taskname_name='taskname_1')
+
+        # create object
+        taskpriority_1 = Taskpriority.objects.create(taskpriority_name='prio_1')
+
+        # create object
+        taskstatus_1 = Taskstatus.objects.create(taskstatus_name='taskstatus_1')
+
+        # create object
+        Task.objects.create(
+            taskname = taskname_1,
+            taskpriority = taskpriority_1,
+            taskstatus = taskstatus_1,
+            system = system_1,
+            task_created_by_user_id = test_user,
+            task_modified_by_user_id = test_user,
+        )
+
+    def test_task_string(self):
+
+        # get object
+        taskname_1 = Taskname.objects.get(taskname_name='taskname_1')
+        # get object
+        task_1 = Task.objects.get(taskname=taskname_1)
+        # compare
+        self.assertEqual(str(task_1), '[' + str(task_1.task_id) + '] ' + str(task_1.taskname) + ' (' + str(task_1.system) + ')')
+
+    def test_task_id_label(self):
+
+        # get object
+        taskname_1 = Taskname.objects.get(taskname_name='taskname_1')
+        # get object
+        task_1 = Task.objects.get(taskname=taskname_1)
+        # get label
+        field_label = task_1._meta.get_field('task_id').verbose_name
+        # compare
+        self.assertEquals(field_label, 'task id')
+
+    def test_task_parent_task_label(self):
+
+        # get object
+        taskname_1 = Taskname.objects.get(taskname_name='taskname_1')
+        # get object
+        task_1 = Task.objects.get(taskname=taskname_1)
+        # get label
+        field_label = task_1._meta.get_field('parent_task').verbose_name
+        # compare
+        self.assertEquals(field_label, 'parent task')
+
+    def test_task_taskname_label(self):
+
+        # get object
+        taskname_1 = Taskname.objects.get(taskname_name='taskname_1')
+        # get object
+        task_1 = Task.objects.get(taskname=taskname_1)
+        # get label
+        field_label = task_1._meta.get_field('taskname').verbose_name
+        # compare
+        self.assertEquals(field_label, 'taskname')
+
+    def test_task_taskpriority_label(self):
+
+        # get object
+        taskname_1 = Taskname.objects.get(taskname_name='taskname_1')
+        # get object
+        task_1 = Task.objects.get(taskname=taskname_1)
+        # get label
+        field_label = task_1._meta.get_field('taskpriority').verbose_name
+        # compare
+        self.assertEquals(field_label, 'taskpriority')
+
+    def test_task_taskstatus_label(self):
+
+        # get object
+        taskname_1 = Taskname.objects.get(taskname_name='taskname_1')
+        # get object
+        task_1 = Task.objects.get(taskname=taskname_1)
+        # get label
+        field_label = task_1._meta.get_field('taskstatus').verbose_name
+        # compare
+        self.assertEquals(field_label, 'taskstatus')
+
+    def test_task_system_label(self):
+
+        # get object
+        taskname_1 = Taskname.objects.get(taskname_name='taskname_1')
+        # get object
+        task_1 = Task.objects.get(taskname=taskname_1)
+        # get label
+        field_label = task_1._meta.get_field('system').verbose_name
+        # compare
+        self.assertEquals(field_label, 'system')
+
+    def test_task_assigned_to_user_id_label(self):
+
+        # get object
+        taskname_1 = Taskname.objects.get(taskname_name='taskname_1')
+        # get object
+        task_1 = Task.objects.get(taskname=taskname_1)
+        # get label
+        field_label = task_1._meta.get_field('task_assigned_to_user_id').verbose_name
+        # compare
+        self.assertEquals(field_label, 'task assigned to user id')
+
+    def test_task_tag_label(self):
+
+        # get object
+        taskname_1 = Taskname.objects.get(taskname_name='taskname_1')
+        # get object
+        task_1 = Task.objects.get(taskname=taskname_1)
+        # get label
+        field_label = task_1._meta.get_field('tag').verbose_name
+        # compare
+        self.assertEquals(field_label, 'tag')
+
+    def test_task_note_label(self):
+
+        # get object
+        taskname_1 = Taskname.objects.get(taskname_name='taskname_1')
+        # get object
+        task_1 = Task.objects.get(taskname=taskname_1)
+        # get label
+        field_label = task_1._meta.get_field('task_note').verbose_name
+        # compare
+        self.assertEquals(field_label, 'task note')
+
+    def test_task_scheduled_time_label(self):
+
+        # get object
+        taskname_1 = Taskname.objects.get(taskname_name='taskname_1')
+        # get object
+        task_1 = Task.objects.get(taskname=taskname_1)
+        # get label
+        field_label = task_1._meta.get_field('task_scheduled_time').verbose_name
+        # compare
+        self.assertEquals(field_label, 'task scheduled time')
+
+    def test_task_started_time_label(self):
+
+        # get object
+        taskname_1 = Taskname.objects.get(taskname_name='taskname_1')
+        # get object
+        task_1 = Task.objects.get(taskname=taskname_1)
+        # get label
+        field_label = task_1._meta.get_field('task_started_time').verbose_name
+        # compare
+        self.assertEquals(field_label, 'task started time')
+
+    def test_task_finished_time_label(self):
+
+        # get object
+        taskname_1 = Taskname.objects.get(taskname_name='taskname_1')
+        # get object
+        task_1 = Task.objects.get(taskname=taskname_1)
+        # get label
+        field_label = task_1._meta.get_field('task_finished_time').verbose_name
+        # compare
+        self.assertEquals(field_label, 'task finished time')
+
+    def test_task_due_time_label(self):
+
+        # get object
+        taskname_1 = Taskname.objects.get(taskname_name='taskname_1')
+        # get object
+        task_1 = Task.objects.get(taskname=taskname_1)
+        # get label
+        field_label = task_1._meta.get_field('task_due_time').verbose_name
+        # compare
+        self.assertEquals(field_label, 'task due time')
+
+    def test_task_create_time_label(self):
+
+        # get object
+        taskname_1 = Taskname.objects.get(taskname_name='taskname_1')
+        # get object
+        task_1 = Task.objects.get(taskname=taskname_1)
+        # get label
+        field_label = task_1._meta.get_field('task_create_time').verbose_name
+        # compare
+        self.assertEquals(field_label, 'task create time')
+
+    def test_task_modify_time_label(self):
+
+        # get object
+        taskname_1 = Taskname.objects.get(taskname_name='taskname_1')
+        # get object
+        task_1 = Task.objects.get(taskname=taskname_1)
+        # get label
+        field_label = task_1._meta.get_field('task_modify_time').verbose_name
+        # compare
+        self.assertEquals(field_label, 'task modify time')
+
+    def test_task_created_by_user_id_label(self):
+
+        # get object
+        taskname_1 = Taskname.objects.get(taskname_name='taskname_1')
+        # get object
+        task_1 = Task.objects.get(taskname=taskname_1)
+        # get label
+        field_label = task_1._meta.get_field('task_created_by_user_id').verbose_name
+        # compare
+        self.assertEquals(field_label, 'task created by user id')
+
+    def test_task_modified_by_user_id_label(self):
+
+        # get object
+        taskname_1 = Taskname.objects.get(taskname_name='taskname_1')
+        # get object
+        task_1 = Task.objects.get(taskname=taskname_1)
+        # get label
+        field_label = task_1._meta.get_field('task_modified_by_user_id').verbose_name
+        # compare
+        self.assertEquals(field_label, 'task modified by user id')
---
+++ dfirtrack_main/tests/task/test_task_views.py
@@ -0,0 +1,218 @@
+from django.contrib.auth.models import User
+from django.test import TestCase
+from dfirtrack_main.models import Task, Taskname, Taskpriority, Taskstatus
+from dfirtrack_main.views import tasks_views
+import urllib.parse
+
+class TaskViewTestCase(TestCase):
+    """ task view tests """
+
+    @classmethod
+    def setUpTestData(cls):
+
+        # create user
+        test_user = User.objects.create_user(username='testuser_task', password='8dR7ilC8cnCr8U2aq14V')
+        test_user.save()
+
+        # create object
+        taskname_1 = Taskname.objects.create(taskname_name='taskname_1')
+
+        # create object
+        taskpriority_1 = Taskpriority.objects.create(taskpriority_name='prio_1')
+
+        # create object
+        taskstatus_1 = Taskstatus.objects.create(taskstatus_name='taskstatus_1')
+
+        # create object
+        Task.objects.create(
+            taskname = taskname_1,
+            taskpriority = taskpriority_1,
+            taskstatus = taskstatus_1,
+            task_created_by_user_id = test_user,
+            task_modified_by_user_id = test_user,
+        )
+
+    def test_tasks_list_not_logged_in(self):
+
+        # create url
+        destination = '/login/?next=' + urllib.parse.quote('/tasks/', safe='')
+        # get response
+        response = self.client.get('/tasks/', follow=True)
+        # compare
+        self.assertRedirects(response, destination, status_code=302, target_status_code=200)
+
+    def test_tasks_list_logged_in(self):
+
+        # login testuser
+        login = self.client.login(username='testuser_task', password='8dR7ilC8cnCr8U2aq14V')
+        # get response
+        response = self.client.get('/tasks/')
+        # compare
+        self.assertEqual(response.status_code, 200)
+
+    def test_tasks_list_template(self):
+
+        # login testuser
+        login = self.client.login(username='testuser_task', password='8dR7ilC8cnCr8U2aq14V')
+        # get response
+        response = self.client.get('/tasks/')
+        # compare
+        self.assertTemplateUsed(response, 'dfirtrack_main/task/tasks_list.html')
+
+    def test_tasks_list_get_user_context(self):
+
+        # login testuser
+        login = self.client.login(username='testuser_task', password='8dR7ilC8cnCr8U2aq14V')
+        # get response
+        response = self.client.get('/tasks/')
+        # compare
+        self.assertEqual(str(response.context['user']), 'testuser_task')
+
+    def test_tasks_detail_not_logged_in(self):
+
+        # get object
+        taskname_1 = Taskname.objects.get(taskname_name='taskname_1')
+        # get object
+        task_1 = Task.objects.get(taskname=taskname_1)
+        # create url
+        destination = '/login/?next=' + urllib.parse.quote('/tasks/' + str(task_1.task_id), safe='')
+        # get response
+        response = self.client.get('/tasks/' + str(task_1.task_id), follow=True)
+        # compare
+        self.assertRedirects(response, destination, status_code=302, target_status_code=200)
+
+    def test_tasks_detail_logged_in(self):
+
+        # get object
+        taskname_1 = Taskname.objects.get(taskname_name='taskname_1')
+        # get object
+        task_1 = Task.objects.get(taskname=taskname_1)
+        # login testuser
+        login = self.client.login(username='testuser_task', password='8dR7ilC8cnCr8U2aq14V')
+        # get response
+        response = self.client.get('/tasks/' + str(task_1.task_id))
+        # compare
+        self.assertEqual(response.status_code, 200)
+
+    def test_tasks_detail_template(self):
+
+        # get object
+        taskname_1 = Taskname.objects.get(taskname_name='taskname_1')
+        # get object
+        task_1 = Task.objects.get(taskname=taskname_1)
+        # login testuser
+        login = self.client.login(username='testuser_task', password='8dR7ilC8cnCr8U2aq14V')
+        # get response
+        response = self.client.get('/tasks/' + str(task_1.task_id))
+        # compare
+        self.assertTemplateUsed(response, 'dfirtrack_main/task/tasks_detail.html')
+
+    def test_tasks_detail_get_user_context(self):
+
+        # get object
+        taskname_1 = Taskname.objects.get(taskname_name='taskname_1')
+        # get object
+        task_1 = Task.objects.get(taskname=taskname_1)
+        # login testuser
+        login = self.client.login(username='testuser_task', password='8dR7ilC8cnCr8U2aq14V')
+        # get response
+        response = self.client.get('/tasks/' + str(task_1.task_id))
+        # compare
+        self.assertEqual(str(response.context['user']), 'testuser_task')
+
+    def test_tasks_add_not_logged_in(self):
+
+        # create url
+        destination = '/login/?next=' + urllib.parse.quote('/tasks/add/', safe='')
+        # get response
+        response = self.client.get('/tasks/add/', follow=True)
+        # compare
+        self.assertRedirects(response, destination, status_code=302, target_status_code=200)
+
+    def test_tasks_add_logged_in(self):
+
+        # login testuser
+        login = self.client.login(username='testuser_task', password='8dR7ilC8cnCr8U2aq14V')
+        # get response
+        response = self.client.get('/tasks/add/')
+        # compare
+        self.assertEqual(response.status_code, 200)
+
+    def test_tasks_add_template(self):
+
+        # login testuser
+        login = self.client.login(username='testuser_task', password='8dR7ilC8cnCr8U2aq14V')
+        # get response
+        response = self.client.get('/tasks/add/')
+        # compare
+        self.assertTemplateUsed(response, 'dfirtrack_main/task/tasks_add.html')
+
+    def test_tasks_add_get_user_context(self):
+
+        # login testuser
+        login = self.client.login(username='testuser_task', password='8dR7ilC8cnCr8U2aq14V')
+        # get response
+        response = self.client.get('/tasks/add/')
+        # compare
+        self.assertEqual(str(response.context['user']), 'testuser_task')
+
+    def test_tasks_edit_not_logged_in(self):
+
+        # get object
+        taskname_1 = Taskname.objects.get(taskname_name='taskname_1')
+        # get object
+        task_1 = Task.objects.get(taskname=taskname_1)
+        # create url
+        destination = '/login/?next=' + urllib.parse.quote('/tasks/' + str(task_1.task_id) + '/edit/', safe='')
+        # get response
+        response = self.client.get('/tasks/' + str(task_1.task_id) + '/edit/', follow=True)
+        # compare
+        self.assertRedirects(response, destination, status_code=302, target_status_code=200)
+
+    def test_tasks_edit_logged_in(self):
+
+        # get object
+        taskname_1 = Taskname.objects.get(taskname_name='taskname_1')
+        # get object
+        task_1 = Task.objects.get(taskname=taskname_1)
+        # login testuser
+        login = self.client.login(username='testuser_task', password='8dR7ilC8cnCr8U2aq14V')
+        # get response
+        response = self.client.get('/tasks/' + str(task_1.task_id) + '/edit/')
+        # compare
+        self.assertEqual(response.status_code, 200)
+
+    def test_tasks_edit_template(self):
+
+        # get object
+        taskname_1 = Taskname.objects.get(taskname_name='taskname_1')
+        # get object
+        task_1 = Task.objects.get(taskname=taskname_1)
+        # login testuser
+        login = self.client.login(username='testuser_task', password='8dR7ilC8cnCr8U2aq14V')
+        # get response
+        response = self.client.get('/tasks/' + str(task_1.task_id) + '/edit/')
+        # compare
+        self.assertTemplateUsed(response, 'dfirtrack_main/task/tasks_edit.html')
+
+    def test_tasks_edit_get_user_context(self):
+
+        # get object
+        taskname_1 = Taskname.objects.get(taskname_name='taskname_1')
+        # get object
+        task_1 = Task.objects.get(taskname=taskname_1)
+        # login testuser
+        login = self.client.login(username='testuser_task', password='8dR7ilC8cnCr8U2aq14V')
+        # get response
+        response = self.client.get('/tasks/' + str(task_1.task_id) + '/edit/')
+        # compare
+        self.assertEqual(str(response.context['user']), 'testuser_task')
+
+#    def test_tasks_detail_logged_in_not_existing(self):
+#
+#        # login testuser
+#        login = self.client.login(username='testuser_task', password='8dR7ilC8cnCr8U2aq14V')
+#        # get response
+#        response = self.client.get('/tasks/x')
+#        # compare
+#        self.assertEqual(response.status_code, 404)
