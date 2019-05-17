07e253c1a42c2acd9b0db0745a34dd7e704638f9
Sven Püschel <spueschel89@gmail.com>
2019-05-17 14:24:48 +0200

    Artifacttype added

--- dfirtrack_artifacts/models.py
+++ dfirtrack_artifacts/models.py
@@ -63,3 +63,35 @@ class Artifactstatus(models.Modell):
             "|artifactstatus_description:" str(artfiactstatus.artifactstatus_description) +
             "|artifactstatus_slug:" str(artfiactstatus.artifactstatus_slug)
         )
+
+class Artifacttype(models.Modell):
+    ''' Artifacttype like File, Registry-Key, Registry-Hive, etc. '''
+
+    # primary key
+    artifacttype_id = models.AutoFieldField(primary_key=True)
+
+    # main entity information
+    artifacttype_name = models.CharField(max_length=255, blank=False, unique=True)
+    artifacttype_description = models.CharField(max_length=2048, blank=False, null=False, unique=True)
+    artifacttype_slug = models.CharField(max_length=255, blank=False, null=False, unique=True)
+
+    # meta information
+    artifacttype_create_time = models.DateTimeField(auto_now_add=True)
+    artifacttype_modify_time = models.DateTimeField(auto_now_add=True)
+    artifacttype_created_by_user_id = models.ForeignKey(User, on_delete=models.Protect, related_name='artifacttype_created_by')
+    artifacttype_modified_by_user_id = models.ForeignKey(User, on_delete=models.Protect, related_name='artifacttype_modified_by')
+
+    # string representation
+    def __str__(self):
+        return 'Artifacstatus {0}'.format(str(self.artifacttype_id))
+
+    #define logger
+    def logger(artifactstatus, request_user, log_text):
+        stdlogger.info(
+            request_user +
+            log_text +
+            " artifacttype_id:" + str(artifactstatus.artifacttype_id) +
+            "|artifacttype_name:" str(artfiactstatus.artifacttype_name) +
+            "|artifacttype_description:" str(artfiactstatus.artifacttype_description) +
+            "|artifacttype_slug:" str(artfiactstatus.artifacttype_slug)
+        )
