f42ec03d3f89c817bd016287e3994c00a36153a5
Sven Püschel <spueschel89@gmail.com>
2019-05-24 20:34:22 +0200

    dfirtrack_artifacts: register models in admin site

--- dfirtrack_artifacts/admin.py
+++ dfirtrack_artifacts/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from dfirtrack_artifacts.models import Artifact, Artifactstatus, Artifacttype
 # Register your models here.
+admin.site.register(Artifact)
+admin.site.register(Artifactstatus)
+admin.site.register(Artifacttype)
