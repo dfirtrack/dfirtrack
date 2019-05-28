52461be0d075fc7437205ae311a8c26ef73a78b4
Sven Püschel <spueschel89@gmail.com>
2019-05-28 21:35:04 +0200

    dfirtrack_artifacts: models.py imported reverse

--- dfirtrack_artifacts/models.py
+++ dfirtrack_artifacts/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.db.models import *
 from django.contrib.auth import get_user_model
+from django.urls import reverse
 from django.contrib.auth.models import User
 import logging
 from time import strftime
