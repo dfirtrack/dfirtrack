94210dde59c4793e42821c55c3475c9e1ebb13a4
Mathias Stuhlmacher <mathias.stuhlmacher@gmx.de>
2019-06-15 10:38:21 +0200

    removed unused imports

--- dfirtrack_main/views/tag_views.py
+++ dfirtrack_main/views/tag_views.py
@@ -1,8 +1,6 @@
 from django.contrib import messages
-from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.http import HttpResponse
-from django.shortcuts import get_object_or_404, redirect, render
+from django.shortcuts import redirect, render
 from django.views.generic import DetailView, ListView
 from django.views.generic.edit import CreateView, DeleteView, UpdateView
 from dfirtrack_main.forms import TagForm
