7a6ea7c9d03757bf81bc54c8d1462f5cb708511b
Mathias Stuhlmacher <mathias.stuhlmacher@gmx.de>
2019-08-27 21:35:02 +0200

    changed form fields arrangement

--- dfirtrack_artifacts/forms.py
+++ dfirtrack_artifacts/forms.py
@@ -6,15 +6,31 @@ from dfirtrack_artifacts.models import Artifact, Artifactstatus, Artifacttype
 class ArtifactForm(forms.ModelForm):
     class Meta:
         model = Artifact
-        fields = ['artifact_id', 'artifact_name', 'artifact_description', 'artifact_storage_path', 'artifacttype', 'case', 'system', 'artifact_acquisition_time']
+        fields = [
+            'artifact_id',
+            'artifact_name',
+            'artifact_description',
+            'artifact_storage_path',
+            'artifacttype',
+            'case',
+            'system',
+            'artifact_acquisition_time',
+        ]
 
 class ArtifactstatusForm(forms.ModelForm):
     class Meta:
         model = Artifactstatus
-        fields = ['artifactstatus_id', 'artifactstatus_name', 'artifactstatus_description']
+        fields = [
+            'artifactstatus_id',
+            'artifactstatus_name',
+            'artifactstatus_description',
+        ]
 
 class ArtifacttypeForm(forms.ModelForm):
     class Meta:
         model = Artifacttype
-        fields = ['artifacttype_id', 'artifacttype_name', 'artifacttype_description']
-
+        fields = [
+            'artifacttype_id',
+            'artifacttype_name',
+            'artifacttype_description',
+        ]
