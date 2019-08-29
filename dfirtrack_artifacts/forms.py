5b83ccf8139b0ce77a376baef5b13677e3f15955
Mathias Stuhlmacher <mathias.stuhlmacher@gmx.de>
2019-08-29 18:09:06 +0200

    dfirtrack_artifacts: changed forms

--- dfirtrack_artifacts/forms.py
+++ dfirtrack_artifacts/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.utils.translation import gettext_lazy
 from dfirtrack_main.models import Case, System
 from dfirtrack_artifacts.models import Artifact, Artifacttype
 
@@ -6,6 +7,7 @@ from dfirtrack_artifacts.models import Artifact, Artifacttype
 class ArtifactForm(forms.ModelForm):
     class Meta:
         model = Artifact
+        # this HTML forms are shown
         fields = [
             'artifact_name',
             'artifact_description',
@@ -16,11 +18,31 @@ class ArtifactForm(forms.ModelForm):
             'case',
             'artifact_acquisition_time',
         ]
+        # non default form labeling
+        labels = {
+            'artifact_name': gettext_lazy('Artifact name (*)'),
+            'artifacttype': gettext_lazy('Artifacttype (*)'),
+            'artifactstatus': gettext_lazy('Artifactstatus (*)'),
+            'system': gettext_lazy('System (*)'),
+        }
+        # special form type or option
+        widgets = {
+            'artifact_name': forms.TextInput(attrs={'autofocus': 'autofocus'}),
+        }
 
 class ArtifacttypeForm(forms.ModelForm):
     class Meta:
         model = Artifacttype
+        # this HTML forms are shown
         fields = [
             'artifacttype_name',
             'artifacttype_description',
         ]
+        # non default form labeling
+        labels = {
+            'artifacttype_name': gettext_lazy('Artifacttype name (*)'),
+        }
+        # special form type or option
+        widgets = {
+            'artifacttype_name': forms.TextInput(attrs={'autofocus': 'autofocus'}),
+        }
