c0c8eff753cd48e69fee77315a82f979b0ff111c
Mathias Stuhlmacher <mathias.stuhlmacher@gmx.de>
2019-08-29 18:23:50 +0200

    dfirtrack_artifacts: updated artifacttype stuff

--- dfirtrack_artifacts/views/artifacttype_view.py
+++ dfirtrack_artifacts/views/artifacttype_view.py
@@ -1,25 +1,39 @@
-from django.shortcuts import render, get_object_or_404, redirect
-from django.views.generic import DetailView, ListView, UpdateView, CreateView, RedirectView
-from django.db import IntegrityError
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.contrib import messages
-from dfirtrack_artifacts import models as artifacts_models
-from dfirtrack_artifacts import forms
+from django.views.generic import DetailView, ListView, UpdateView, CreateView
+from dfirtrack_artifacts.models import Artifacttype
+from dfirtrack_artifacts.forms import ArtifacttypeForm
+from dfirtrack_main.logger.default_logger import debug_logger
 
-class ArtifacttypeListView(ListView):
-    model = artifacts_models.Artifacttype
+class ArtifacttypeListView(LoginRequiredMixin, ListView):
+    login_url = '/login'
+    model = Artifacttype
     template_name = 'dfirtrack_artifacts/artifacttype/artifacttype_list.html'
+    context_object_name = 'artifacttype_list'
 
-class ArtifacttypeCreateView(CreateView):
-    model = artifacts_models.Artifacttype
-    form_class = forms.ArtifacttypeForm
-    template_name = 'dfirtrack_artifacts/artifacttype/artifacttype_add.html'
+    def get_queryset(self):
+        debug_logger(str(self.request.user), " ARTIFACTTYPE_LIST_ENTERED")
+        return Artifacttype.objects.order_by('artifacttype_name')
 
-class ArtifacttypeDetailView(DetailView):
-    model = artifacts_models.Artifacttype
+class ArtifacttypeDetailView(LoginRequiredMixin, DetailView):
+    login_url = '/login'
+    model = Artifacttype
     template_name = 'dfirtrack_artifacts/artifacttype/artifacttype_detail.html'
 
-class ArtifacttypeUpdateView(UpdateView):
-    model = artifacts_models.Artifacttype
-    form_class = forms.ArtifacttypeForm
+    # TODO: does not work as expected (and in contrast to dfirtrack_main.views)
+    #def get_context_data(self, **kwargs):
+    #    context = super().get_context_data(**kwargs)
+    #    artifacttype = self.object
+    #    artifacttype.logger(str(self.request.user), " ARTIFACTTYPE_DETAIL_ENTERED")
+    #    return context
+
+class ArtifacttypeCreateView(LoginRequiredMixin, CreateView):
+    login_url = '/login'
+    model = Artifacttype
+    form_class = ArtifacttypeForm
+    template_name = 'dfirtrack_artifacts/artifacttype/artifacttype_add.html'
+
+class ArtifacttypeUpdateView(LoginRequiredMixin, UpdateView):
+    login_url = '/login'
+    model = Artifacttype
+    form_class = ArtifacttypeForm
     template_name = 'dfirtrack_artifacts/artifacttype/artifacttype_edit.html'
