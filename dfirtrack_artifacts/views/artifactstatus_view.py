b68b9e0b917d816952ceb9f6074d0ef01dd56600
Mathias Stuhlmacher <mathias.stuhlmacher@gmx.de>
2019-08-29 18:29:01 +0200

    dfirtrack_artifacts: updated artifactstatus views

--- dfirtrack_artifacts/views/artifactstatus_view.py
+++ dfirtrack_artifacts/views/artifactstatus_view.py
@@ -1,14 +1,26 @@
-from django.views.generic import DetailView, ListView
 from django.contrib.auth.mixins import LoginRequiredMixin
-from dfirtrack_artifacts import models as artifacts_models
+from django.views.generic import DetailView, ListView
+from dfirtrack_artifacts.models import Artifactstatus
+from dfirtrack_main.logger.default_logger import debug_logger
 
 class ArtifactstatusListView(LoginRequiredMixin, ListView):
     login_url = '/login'
-    model = artifacts_models.Artifactstatus
+    model = Artifactstatus
     template_name = 'dfirtrack_artifacts/artifactstatus/artifactstatus_list.html'
     context_object_name = 'artifactstatus_list'
 
+    def get_queryset(self):
+        debug_logger(str(self.request.user), ' ARTIFACTSTATUS_LIST_ENTERED')
+        return Artifactstatus.objects.order_by('artifactstatus_id')
+
 class ArtifactstatusDetailView(LoginRequiredMixin, DetailView):
     login_url = '/login'
-    model = artifacts_models.Artifactstatus
+    model = Artifactstatus
     template_name = 'dfirtrack_artifacts/artifactstatus/artifactstatus_detail.html'
+
+    # TODO: does not work as expected (and in contrast to dfirtrack_main.views)
+    #def get_context_data(self, **kwargs):
+    #    context = super().get_context_data(**kwargs)
+    #    artifactstatus = self.object
+    #    artifactstatus.logger(str(self.request.user), ' ARTIFACTSTATUSDETAIL_ENTERED')
+    #    return context
