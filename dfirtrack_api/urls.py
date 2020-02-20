307b6e59ca461fdad72664f5def37d60d70c17b0
Mathias Stuhlmacher <mathias.stuhlmacher@gmx.de>
2020-02-20 21:28:49 +0100

    restructured API

--- dfirtrack_api/urls.py
+++ dfirtrack_api/urls.py
@@ -1,18 +1,18 @@
 from django.conf.urls import url
 from rest_framework.urlpatterns import format_suffix_patterns
-from . import views, views_dfirtrack_artifacts
+from dfirtrack_api.views import dfirtrack_artifacts, dfirtrack_main
 
 urlpatterns = [
 
     # dfirtrack_artifacts
-    url(r'^artifacts/$', views_dfirtrack_artifacts.ArtifactListApi.as_view()),
+    url(r'^artifacts/$', dfirtrack_artifacts.ArtifactListApi.as_view()),
 
     # dfirtrack_main
-    url(r'^ips/$', views.IpListApi.as_view()),
-    url(r'^oss/$', views.OsListApi.as_view()),
-    url(r'^systems/$', views.SystemListApi.as_view()),
-    url(r'^systems/(?P<pk>\d+)/$', views.SystemDetailApi.as_view()),
-    url(r'^tags/$', views.TagListApi.as_view()),
+    url(r'^ips/$', dfirtrack_main.IpListApi.as_view()),
+    url(r'^oss/$', dfirtrack_main.OsListApi.as_view()),
+    url(r'^systems/$', dfirtrack_main.SystemListApi.as_view()),
+    url(r'^systems/(?P<pk>\d+)/$', dfirtrack_main.SystemDetailApi.as_view()),
+    url(r'^tags/$', dfirtrack_main.TagListApi.as_view()),
 
 ]
 
--- dfirtrack_api/views/dfirtrack_main.py
+++ dfirtrack_api/views/dfirtrack_main.py
@@ -1,4 +1,4 @@
-from . import serializers
+from dfirtrack_api.serializers import dfirtrack_main
 from dfirtrack_main.models import Ip, Os, System, Tag
 from django.http import Http404
 from rest_framework import status
@@ -13,13 +13,13 @@ class IpListApi(APIView):
         # get all objects
         ip = Ip.objects.all()
         # create serializer for all objects
-        serializer = serializers.IpSerializer(ip, many=True)
+        serializer = dfirtrack_main.IpSerializer(ip, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
     def post(self, request, format=None):
         """ define API view for POST request """
 
-        serializer = serializers.IpSerializer(data=request.data)
+        serializer = dfirtrack_main.IpSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
@@ -33,13 +33,13 @@ class OsListApi(APIView):
         # get all objects
         os = Os.objects.all()
         # create serializer for all objects
-        serializer = serializers.OsSerializer(os, many=True)
+        serializer = dfirtrack_main.OsSerializer(os, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
     def post(self, request, format=None):
         """ define API view for POST request """
 
-        serializer = serializers.OsSerializer(data=request.data)
+        serializer = dfirtrack_main.OsSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
@@ -54,7 +54,7 @@ class SystemListApi(APIView):
         # get all objects
         system = System.objects.all()
         # create serializer for all objects
-        serializer = serializers.SystemSerializer(system, many=True)
+        serializer = dfirtrack_main.SystemSerializer(system, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
 class SystemDetailApi(APIView):
@@ -66,7 +66,7 @@ class SystemDetailApi(APIView):
         try:
             system = System.objects.get(system_id = pk)
             # create serializer for single object
-            serializer = serializers.SystemSerializer(system)
+            serializer = dfirtrack_main.SystemSerializer(system)
             return Response(serializer.data)
         except System.DoesNotExist:
             raise Http404
@@ -79,5 +79,5 @@ class TagListApi(APIView):
         # get all objects
         tag = Tag.objects.all()
         # create serializer for all objects
-        serializer = serializers.TagSerializer(tag, many=True)
+        serializer = dfirtrack_main.TagSerializer(tag, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
