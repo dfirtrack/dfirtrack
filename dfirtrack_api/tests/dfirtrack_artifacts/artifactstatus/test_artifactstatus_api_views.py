ee59fa6a4624757f3280a09259d67257d8cedac4
Mathias Stuhlmacher <mathias.stuhlmacher@gmx.de>
2020-02-22 19:38:03 +0100

    so far finished artifactstatus API test

--- dfirtrack_api/tests/dfirtrack_artifacts/artifactstatus/test_artifactstatus_api_views.py
+++ dfirtrack_api/tests/dfirtrack_artifacts/artifactstatus/test_artifactstatus_api_views.py
@@ -14,7 +14,18 @@ class ArtifactstatusAPIViewTestCase(TestCase):
         # create user
         test_user = User.objects.create_user(username='testuser_artifactstatus_api', password='aCTVRIdJ4cyVSkYiJKrM')
 
+# TODO: adapt to headless / "browserless" functionality
+#    def test_artifactstatus_list_api_not_logged_in(self):
+#
+#        # create url
+#        destination = '/login/?next=' + urllib.parse.quote('/api/artifactstatuss/', safe='/')
+#        # get response
+#        response = self.client.get('/api/artifactstatuss/', follow=True)
+#        # compare
+#        self.assertRedirects(response, destination, status_code=302, target_status_code=200)
+
     def test_artifactstatus_list_api_logged_in(self):
+        """ GET is allowed """
 
         # login testuser
         login = self.client.login(username='testuser_artifactstatus_api', password='aCTVRIdJ4cyVSkYiJKrM')
@@ -24,6 +35,7 @@ class ArtifactstatusAPIViewTestCase(TestCase):
         self.assertEqual(response.status_code, 200)
 
     def test_artifactstatus_list_api_redirect(self):
+        """ test redirect """
 
         # login testuser
         login = self.client.login(username='testuser_artifactstatus_api', password='aCTVRIdJ4cyVSkYiJKrM')
@@ -34,6 +46,31 @@ class ArtifactstatusAPIViewTestCase(TestCase):
         # compare
         self.assertRedirects(response, destination, status_code=301, target_status_code=200)
 
+    def test_artifactstatus_list_api_method_post(self):
+        """ POST is allowed """
+
+        # login testuser
+        login = self.client.login(username='testuser_artifactstatus_api', password='aCTVRIdJ4cyVSkYiJKrM')
+        # create url
+        destination = urllib.parse.quote('/api/artifactstatuss/', safe='/')
+        # create POST string
+        poststring = {"artifactstatus_name": "artifactstatus_api_2"}
+        # get response
+        response = self.client.post('/api/artifactstatuss/', data=poststring)
+        # compare
+        self.assertEqual(response.status_code, 201)
+
+    def test_artifactstatus_list_api_get_user_context(self):
+        """ test list view """
+
+        # login testuser
+        login = self.client.login(username='testuser_artifactstatus_api', password='aCTVRIdJ4cyVSkYiJKrM')
+        # get response
+        response = self.client.get('/artifacts/artifactstatus/')
+        # compare
+        self.assertEqual(str(response.context['user']), 'testuser_artifactstatus_api')
+
+# TODO: adapt to headless / "browserless" functionality
 #    def test_artifactstatus_detail_api_not_logged_in(self):
 #
 #        # get object
@@ -46,6 +83,7 @@ class ArtifactstatusAPIViewTestCase(TestCase):
 #        self.assertRedirects(response, destination, status_code=302, target_status_code=200)
 
     def test_artifactstatus_detail_api_logged_in(self):
+        """ GET is allowed """
 
         # get object
         artifactstatus_api_1 = Artifactstatus.objects.get(artifactstatus_name='artifactstatus_api_1')
@@ -57,6 +95,7 @@ class ArtifactstatusAPIViewTestCase(TestCase):
         self.assertEqual(response.status_code, 200)
 
     def test_artifactstatus_detail_api_redirect(self):
+        """ test redirect """
 
         # get object
         artifactstatus_api_1 = Artifactstatus.objects.get(artifactstatus_name='artifactstatus_api_1')
@@ -69,7 +108,8 @@ class ArtifactstatusAPIViewTestCase(TestCase):
         # compare
         self.assertRedirects(response, destination, status_code=301, target_status_code=200)
 
-    def test_artifactstatus_detail_api_delete(self):
+    def test_artifactstatus_detail_api_method_delete(self):
+        """ DELETE is forbidden """
 
         # login testuser
         login = self.client.login(username='testuser_artifactstatus_api', password='aCTVRIdJ4cyVSkYiJKrM')
@@ -80,36 +120,14 @@ class ArtifactstatusAPIViewTestCase(TestCase):
         # compare
         self.assertEqual(response.status_code, 405)
 
-#    def test_artifactstatus_list_api_get_user_context(self):
-#        """ test list view """
-#
-#        # login testuser
-#        login = self.client.login(username='testuser_artifactstatus_api', password='aCTVRIdJ4cyVSkYiJKrM')
-#        # get response
-#        response = self.client.get('/artifacts/artifactstatus/')
-#        # compare
-#        self.assertEqual(str(response.context['user']), 'testuser_artifactstatus_api')
-#
-#    def test_artifactstatus_detail_api_not_logged_in(self):
-#        """ test detail view """
-#
-#        # get object
-#        artifactstatus_api_1 = Artifactstatus.objects.get(artifactstatus_name='artifactstatus_api_1')
-#        # create url
-#        destination = '/login/?next=' + urllib.parse.quote('/artifacts/artifactstatus/detail/' + str(artifactstatus_api_1.artifactstatus_id) + '/', safe='')
-#        # get response
-#        response = self.client.get('/artifacts/artifactstatus/detail/' + str(artifactstatus_api_1.artifactstatus_id) + '/', follow=True)
-#        # compare
-#        self.assertRedirects(response, destination, status_code=302, target_status_code=200)
-#
-#    def test_artifactstatus_detail_api_get_user_context(self):
-#        """ test detail view """
-#
-#        # get object
-#        artifactstatus_api_1 = Artifactstatus.objects.get(artifactstatus_name='artifactstatus_api_1')
-#        # login testuser
-#        login = self.client.login(username='testuser_artifactstatus_api', password='aCTVRIdJ4cyVSkYiJKrM')
-#        # get response
-#        response = self.client.get('/artifacts/artifactstatus/detail/' + str(artifactstatus_api_1.artifactstatus_id) + '/')
-#        # compare
-#        self.assertEqual(str(response.context['user']), 'testuser_artifactstatus_api')
+    def test_artifactstatus_detail_api_get_user_context(self):
+        """ test detail view """
+
+        # get object
+        artifactstatus_api_1 = Artifactstatus.objects.get(artifactstatus_name='artifactstatus_api_1')
+        # login testuser
+        login = self.client.login(username='testuser_artifactstatus_api', password='aCTVRIdJ4cyVSkYiJKrM')
+        # get response
+        response = self.client.get('/artifacts/artifactstatus/detail/' + str(artifactstatus_api_1.artifactstatus_id) + '/')
+        # compare
+        self.assertEqual(str(response.context['user']), 'testuser_artifactstatus_api')
