dc22fbcccbcc667e69cf2c0205a4e45de71d0eca
Mathias Stuhlmacher <mathias.stuhlmacher@gmx.de>
2020-06-15 22:19:49 +0200

    added system-sorting in artifact forms

--- dfirtrack_artifacts/forms.py
+++ dfirtrack_artifacts/forms.py
@@ -5,8 +5,18 @@ from dfirtrack_artifacts.models import Artifact, Artifacttype
 
 
 class ArtifactForm(forms.ModelForm):
+
+    # reorder field choices
+    system = forms.ModelChoiceField(
+        label = gettext_lazy('System (*)'),
+        queryset = System.objects.order_by('system_name'),
+    )
+
     class Meta:
+
+        # model
         model = Artifact
+
         # this HTML forms are shown
         fields = [
             'artifact_name',
@@ -22,18 +32,19 @@ class ArtifactForm(forms.ModelForm):
             'artifact_sha256',
             'artifact_note',
         ]
+
         # non default form labeling
         labels = {
             'artifact_name': gettext_lazy('Artifact name (*)'),
             'artifactstatus': gettext_lazy('Artifactstatus (*)'),
             'artifacttype': gettext_lazy('Artifacttype (*)'),
-            'system': gettext_lazy('System (*)'),
             'artifact_requested_time': gettext_lazy('Artifact requested time (YYYY-MM-DD HH:MM:SS)'),
             'artifact_acquisition_time': gettext_lazy('Artifact acquisition time (YYYY-MM-DD HH:MM:SS)'),
             'artifact_md5': gettext_lazy('MD5'),
             'artifact_sha1': gettext_lazy('SHA1'),
             'artifact_sha256': gettext_lazy('SHA256'),
         }
+
         # special form type or option
         widgets = {
             'artifact_name': forms.TextInput(attrs={'autofocus': 'autofocus'}),
@@ -87,17 +98,23 @@ class ArtifactForm(forms.ModelForm):
         return self.cleaned_data
 
 class ArtifacttypeForm(forms.ModelForm):
+
     class Meta:
+
+        # model
         model = Artifacttype
+
         # this HTML forms are shown
         fields = [
             'artifacttype_name',
             'artifacttype_note',
         ]
+
         # non default form labeling
         labels = {
             'artifacttype_name': gettext_lazy('Artifacttype name (*)'),
         }
+
         # special form type or option
         widgets = {
             'artifacttype_name': forms.TextInput(attrs={'autofocus': 'autofocus'}),
