2e57989bc72eee8d5d5323a905d93eba0bdbe51b
Mathias Stuhlmacher <mathias.stuhlmacher@gmx.de>
2020-07-12 22:25:31 +0200

    removed imports

--- dfirtrack_main/exporter/spreadsheet/spreadsheet_config_editor.py
+++ dfirtrack_main/exporter/spreadsheet/spreadsheet_config_editor.py
@@ -1,8 +1,7 @@
 from constance import config
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
-from django.shortcuts import redirect, render
-from django.urls import reverse
+from django.shortcuts import render
 from dfirtrack_main.config_forms import SystemExporterSpreadsheetCsvForm, SystemExporterSpreadsheetXlsForm
 
 @login_required(login_url="/login")
