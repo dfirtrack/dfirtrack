e22bfcd536bd54f02f191fd7eafa4dc1e679aafa
Mathias Stuhlmacher <mathias.stuhlmacher@gmx.de>
2020-08-06 17:14:40 +0200

    added tests

--- dfirtrack_main/tests/system/test_system_importer_file_csv_form_based_config_forms.py
+++ dfirtrack_main/tests/system/test_system_importer_file_csv_form_based_config_forms.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from dfirtrack_main.config_forms import SystemImporterFileCsvFormbasedConfigForm
+from dfirtrack_main.models import Analysisstatus, Systemstatus
 
 class SystemImporterFileCsvFormbasedConfigFormTestCase(TestCase):
     """ system importer file CSV config-based config form tests """
@@ -76,10 +77,71 @@ class SystemImporterFileCsvFormbasedConfigFormTestCase(TestCase):
         # compare
         self.assertEqual(form.fields['csv_remove_tag'].label, 'Remove / overwrite existing tags for already existing systems')
 
-#    def test_system_importer_file_csv_form_based_config_form_empty(self):
-#        """ test minimum form requirements / VALID """
-#
-#        # get object
-#        form = SystemImporterFileCsvFormbasedConfigForm(data = {})
-#        # compare
-#        self.assertTrue(form.is_valid())
+    def test_system_importer_file_csv_form_based_config_form_empty(self):
+        """ test minimum form requirements / INVALID """
+
+        # get object
+        form = SystemImporterFileCsvFormbasedConfigForm(data = {})
+        # compare
+        self.assertFalse(form.is_valid())
+
+    def test_system_importer_file_csv_form_based_config_form_systemstatus_filled(self):
+        """ test minimum form requirements / INVALID """
+
+        # get object (does not work the usual way because form with available choices is build before model instance is created during the test)
+        systemstatus_id = Systemstatus.objects.get(systemstatus_name='Unknown').systemstatus_id
+        # get object
+        form = SystemImporterFileCsvFormbasedConfigForm(data = {
+            'csv_default_systemstatus': systemstatus_id,
+        })
+        # compare
+        self.assertFalse(form.is_valid())
+
+    def test_system_importer_file_csv_form_based_config_form_analysisstatus_filled(self):
+        """ test minimum form requirements / INVALID """
+
+        # get object (does not work the usual way because form with available choices is build before model instance is created during the test)
+        systemstatus_id = Systemstatus.objects.get(systemstatus_name='Unknown').systemstatus_id
+        # get object (does not work the usual way because form with available choices is build before model instance is created during the test)
+        analysisstatus_id = Analysisstatus.objects.get(analysisstatus_name='Needs analysis').analysisstatus_id
+        # get object
+        form = SystemImporterFileCsvFormbasedConfigForm(data = {
+            'csv_default_systemstatus': systemstatus_id,
+            'csv_default_analysisstatus': analysisstatus_id,
+        })
+        # compare
+        self.assertFalse(form.is_valid())
+
+    def test_system_importer_file_csv_form_based_config_form_same_columns(self):
+        """ test field validation / INVALID """
+
+        # get object (does not work the usual way because form with available choices is build before model instance is created during the test)
+        systemstatus_id = Systemstatus.objects.get(systemstatus_name='Unknown').systemstatus_id
+        # get object (does not work the usual way because form with available choices is build before model instance is created during the test)
+        analysisstatus_id = Analysisstatus.objects.get(analysisstatus_name='Needs analysis').analysisstatus_id
+        # get object
+        form = SystemImporterFileCsvFormbasedConfigForm(data = {
+            'csv_default_systemstatus': systemstatus_id,
+            'csv_default_analysisstatus': analysisstatus_id,
+            'csv_column_system': 1,
+            'csv_column_ip': 1,
+        })
+        # compare
+        self.assertFalse(form.is_valid())
+
+    def test_system_importer_file_csv_form_based_config_form_different_columns(self):
+        """ test field validation / VALID """
+
+        # get object (does not work the usual way because form with available choices is build before model instance is created during the test)
+        systemstatus_id = Systemstatus.objects.get(systemstatus_name='Unknown').systemstatus_id
+        # get object (does not work the usual way because form with available choices is build before model instance is created during the test)
+        analysisstatus_id = Analysisstatus.objects.get(analysisstatus_name='Needs analysis').analysisstatus_id
+        # get object
+        form = SystemImporterFileCsvFormbasedConfigForm(data = {
+            'csv_default_systemstatus': systemstatus_id,
+            'csv_default_analysisstatus': analysisstatus_id,
+            'csv_column_system': 1,
+            'csv_column_ip': 2,
+        })
+        # compare
+        self.assertTrue(form.is_valid())
