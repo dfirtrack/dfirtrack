6bd92bfed5fdcf754c5addfa976a64e2a98145f9
Mathias Stuhlmacher <mathias.stuhlmacher@gmx.de>
2020-09-24 01:21:44 +0200

    slightly changed branch dependencies

--- dfirtrack_main/templatetags/dfirtrack_main_tags.py
+++ dfirtrack_main/templatetags/dfirtrack_main_tags.py
@@ -9,17 +9,21 @@ def dfirtrack_version():
     versionnumber = 'v0.4.0'
     return versionnumber
 
-@register.simple_tag
-def github_ci():
-    if "CI" in environ:
-        ci = False
-    else:
-        ci = True
-    return ci
+"""
+following conditions are necessary for Pull Requests
 
+GitHub actions do some kind of `git checkout` on PRs, which causes
+'TypeError: HEAD is a detached symbolic reference as it points to...'
+
+multiple issues exists on this in https://github.com/gitpython-developers/GitPython
+actually solved, but does still not work here
+"""
+
+# check for GitHub action
 if not "CI" in environ:
     @register.simple_tag
     def dfirtrack_branch():
+        # not in GitHub action --> get and return current branch
         working_dir = getcwd()
         repo = Repo(working_dir)
         branch = repo.active_branch
@@ -27,4 +31,16 @@ if not "CI" in environ:
 else:
     @register.simple_tag
     def dfirtrack_branch():
+        # in GitHub action --> return dummy value to avoid errors
         return "unknown"
+
+# check for GitHub action
+@register.simple_tag
+def github_ci():
+    if not "CI" in environ:
+        # not in GitHub action --> show branch in maintemplate
+        ci = False
+    else:
+        # in GitHub action --> do not show branch in maintemplate
+        ci = True
+    return ci
