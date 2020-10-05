f5ac9beb5c5721280d80d02e80cb9a148a8dbdfb
Mathias Stuhlmacher <mathias.stuhlmacher@gmx.de>
2020-10-05 23:22:09 +0200

    added systemtype tests

--- dfirtrack_main/tests/systemtype/test_systemtype_views.py
+++ dfirtrack_main/tests/systemtype/test_systemtype_views.py
@@ -210,6 +210,112 @@ class SystemtypeViewTestCase(TestCase):
         # compare
         self.assertEqual(response.status_code, 200)
 
+    def test_systemtype_add_post_invalid_template(self):
+        """ test add view """
+
+        # login testuser
+        login = self.client.login(username='testuser_systemtype', password='A8VfAc8hrJp3Dg7EtMpu')
+        # create post data
+        data_dict = {}
+        # get response
+        response = self.client.post('/systemtype/add/', data_dict)
+        # compare
+        self.assertTemplateUsed(response, 'dfirtrack_main/systemtype/systemtype_add.html')
+
+    def test_systemtype_add_popup_not_logged_in(self):
+        """ test add view """
+
+        # create url
+        destination = '/login/?next=' + urllib.parse.quote('/systemtype/add_popup/', safe='')
+        # get response
+        response = self.client.get('/systemtype/add_popup/', follow=True)
+        # compare
+        self.assertRedirects(response, destination, status_code=302, target_status_code=200)
+
+    def test_systemtype_add_popup_logged_in(self):
+        """ test add view """
+
+        # login testuser
+        login = self.client.login(username='testuser_systemtype', password='A8VfAc8hrJp3Dg7EtMpu')
+        # get response
+        response = self.client.get('/systemtype/add_popup/')
+        # compare
+        self.assertEqual(response.status_code, 200)
+
+    def test_systemtype_add_popup_template(self):
+        """ test add view """
+
+        # login testuser
+        login = self.client.login(username='testuser_systemtype', password='A8VfAc8hrJp3Dg7EtMpu')
+        # get response
+        response = self.client.get('/systemtype/add_popup/')
+        # compare
+        self.assertTemplateUsed(response, 'dfirtrack_main/systemtype/systemtype_add_popup.html')
+
+    def test_systemtype_add_popup_get_user_context(self):
+        """ test add view """
+
+        # login testuser
+        login = self.client.login(username='testuser_systemtype', password='A8VfAc8hrJp3Dg7EtMpu')
+        # get response
+        response = self.client.get('/systemtype/add_popup/')
+        # compare
+        self.assertEqual(str(response.context['user']), 'testuser_systemtype')
+
+    def test_systemtype_add_popup_redirect(self):
+        """ test add view """
+
+        # login testuser
+        login = self.client.login(username='testuser_systemtype', password='A8VfAc8hrJp3Dg7EtMpu')
+        # create url
+        destination = urllib.parse.quote('/systemtype/add_popup/', safe='/')
+        # get response
+        response = self.client.get('/systemtype/add_popup', follow=True)
+        # compare
+        self.assertRedirects(response, destination, status_code=301, target_status_code=200)
+
+    def test_systemtype_add_popup_post_redirect(self):
+        """ test add view """
+
+        # login testuser
+        login = self.client.login(username='testuser_systemtype', password='A8VfAc8hrJp3Dg7EtMpu')
+        # create post data
+        data_dict = {
+            'systemtype_name': 'systemtype_add_popup_post_test',
+        }
+        # get response
+        response = self.client.post('/systemtype/add_popup/', data_dict)
+        # get object
+        systemtype_id = Systemtype.objects.get(systemtype_name = 'systemtype_add_popup_post_test').systemtype_id
+        # create url
+        destination = urllib.parse.quote('/systemtype/' + str(systemtype_id) + '/', safe='/')
+        # compare
+        self.assertEqual(response.status_code, 200)
+
+    def test_systemtype_add_popup_post_invalid_reload(self):
+        """ test add view """
+
+        # login testuser
+        login = self.client.login(username='testuser_systemtype', password='A8VfAc8hrJp3Dg7EtMpu')
+        # create post data
+        data_dict = {}
+        # get response
+        response = self.client.post('/systemtype/add_popup/', data_dict)
+        # compare
+        self.assertEqual(response.status_code, 200)
+
+    def test_systemtype_add_popup_post_invalid_template(self):
+        """ test add view """
+
+        # login testuser
+        login = self.client.login(username='testuser_systemtype', password='A8VfAc8hrJp3Dg7EtMpu')
+        # create post data
+        data_dict = {}
+        # get response
+        response = self.client.post('/systemtype/add_popup/', data_dict)
+        # compare
+        self.assertTemplateUsed(response, 'dfirtrack_main/systemtype/systemtype_add_popup.html')
+
     def test_systemtype_edit_not_logged_in(self):
         """ test edit view """
 
@@ -305,3 +411,17 @@ class SystemtypeViewTestCase(TestCase):
         response = self.client.post('/systemtype/' + str(systemtype_id) + '/edit/', data_dict)
         # compare
         self.assertEqual(response.status_code, 200)
+
+    def test_systemtype_edit_post_invalid_template(self):
+        """ test edit view """
+
+        # login testuser
+        login = self.client.login(username='testuser_systemtype', password='A8VfAc8hrJp3Dg7EtMpu')
+        # get object
+        systemtype_id = Systemtype.objects.get(systemtype_name='systemtype_1').systemtype_id
+        # create post data
+        data_dict = {}
+        # get response
+        response = self.client.post('/systemtype/' + str(systemtype_id) + '/edit/', data_dict)
+        # compare
+        self.assertTemplateUsed(response, 'dfirtrack_main/systemtype/systemtype_edit.html')
