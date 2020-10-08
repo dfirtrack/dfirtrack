bc01e044cae4ab5dedc602de74071f477630191d
Mathias Stuhlmacher <mathias.stuhlmacher@gmx.de>
2020-10-08 21:46:59 +0200

    started additional task tests

--- dfirtrack_main/tests/task/test_task_views.py
+++ dfirtrack_main/tests/task/test_task_views.py
@@ -206,6 +206,62 @@ class TaskViewTestCase(TestCase):
         # compare
         self.assertRedirects(response, destination, status_code=301, target_status_code=200)
 
+    def test_task_add_post_redirect(self):
+        """ test add view """
+
+        # login testuser
+        login = self.client.login(username='testuser_task', password='8dR7ilC8cnCr8U2aq14V')
+        # get user
+        test_user_id = User.objects.get(username = 'testuser_task').id
+        # get object
+        taskname_id = Taskname.objects.create(taskname_name = 'task_add_post_test').taskname_id
+        # get object
+        taskpriority_id = Taskpriority.objects.get(taskpriority_name = 'prio_1').taskpriority_id
+        # get object
+        taskstatus_id = Taskstatus.objects.get(taskstatus_name = 'taskstatus_1').taskstatus_id
+        # get post data
+        data_dict = {
+            'taskname': taskname_id,
+            'taskpriority': taskpriority_id,
+            'taskstatus': taskstatus_id,
+            'task_created_by_user_id': test_user_id,
+            'task_modified_by_user_id': test_user_id,
+        }
+        # get response
+        response = self.client.post('/task/add/', data_dict)
+        # get object
+        taskname = Taskname.objects.get(taskname_name = 'task_add_post_test')
+        # get object
+        task_id = Task.objects.get(taskname = taskname).task_id
+        # create url
+        destination = urllib.parse.quote('/task/' + str(task_id) + '/', safe='/')
+        # compare
+        self.assertRedirects(response, destination, status_code=302, target_status_code=200)
+
+    def test_task_add_post_invalid_reload(self):
+        """ test add view """
+
+        # login testuser
+        login = self.client.login(username='testuser_task', password='8dR7ilC8cnCr8U2aq14V')
+        # create post data
+        data_dict = {}
+        # get response
+        response = self.client.post('/task/add/', data_dict)
+        # compare
+        self.assertEqual(response.status_code, 200)
+
+    def test_task_add_post_invalid_template(self):
+        """ test add view """
+
+        # login testuser
+        login = self.client.login(username='testuser_task', password='8dR7ilC8cnCr8U2aq14V')
+        # create post data
+        data_dict = {}
+        # get response
+        response = self.client.post('/task/add/', data_dict)
+        # compare
+        self.assertTemplateUsed(response, 'dfirtrack_main/task/task_add.html')
+
     def test_task_edit_not_logged_in(self):
         """ test edit view """
 
@@ -277,3 +333,74 @@ class TaskViewTestCase(TestCase):
         response = self.client.get('/task/' + str(task_1.task_id) + '/edit', follow=True)
         # compare
         self.assertRedirects(response, destination, status_code=301, target_status_code=200)
+
+    def test_task_edit_post_redirect(self):
+        """ test edit view """
+
+        # login testuser
+        login = self.client.login(username='testuser_task', password='8dR7ilC8cnCr8U2aq14V')
+        # get user
+        test_user = User.objects.get(username = 'testuser_task')
+        # get object
+        taskname_1 = Taskname.objects.create(taskname_name = 'task_edit_post_test_1')
+        # get object
+        taskname_2 = Taskname.objects.create(taskname_name = 'task_edit_post_test_2')
+        # get object
+        taskpriority = Taskpriority.objects.get(taskpriority_name = 'prio_1')
+        # get object
+        taskstatus = Taskstatus.objects.get(taskstatus_name = 'taskstatus_1')
+        # create object
+        task_1 = Task.objects.create(
+            taskname = taskname_1,
+            taskpriority = taskpriority,
+            taskstatus = taskstatus,
+            task_created_by_user_id = test_user,
+            task_modified_by_user_id = test_user,
+        )
+        # create post data
+        data_dict = {
+            'taskname': taskname_2.taskname_id,
+            'taskpriority': taskpriority.taskpriority_id,
+            'taskstatus': taskstatus.taskstatus_id,
+            'task_modified_by_user_id': test_user.id,
+        }
+        # get response
+        response = self.client.post('/task/' + str(task_1.task_id) + '/edit/', data_dict)
+        # get object
+        task_2 = Task.objects.get(taskname = taskname_2)
+        # create url
+        destination = urllib.parse.quote('/task/' + str(task_2.task_id) + '/', safe='/')
+        # compare
+        self.assertRedirects(response, destination, status_code=302, target_status_code=200)
+
+    def test_task_edit_post_invalid_reload(self):
+        """ test edit view """
+
+        # login testuser
+        login = self.client.login(username='testuser_task', password='8dR7ilC8cnCr8U2aq14V')
+        # get object
+        taskname_1 = Taskname.objects.get(taskname_name = 'taskname_1')
+        # get object
+        task_id = Task.objects.get(taskname = taskname_1).task_id
+        # create post data
+        data_dict = {}
+        # get response
+        response = self.client.post('/task/' + str(task_id) + '/edit/', data_dict)
+        # compare
+        self.assertEqual(response.status_code, 200)
+
+    def test_task_edit_post_invalid_template(self):
+        """ test edit view """
+
+        # login testuser
+        login = self.client.login(username='testuser_task', password='8dR7ilC8cnCr8U2aq14V')
+        # get object
+        taskname_1 = Taskname.objects.get(taskname_name = 'taskname_1')
+        # get object
+        task_id = Task.objects.get(taskname = taskname_1).task_id
+        # create post data
+        data_dict = {}
+        # get response
+        response = self.client.post('/task/' + str(task_id) + '/edit/', data_dict)
+        # compare
+        self.assertTemplateUsed(response, 'dfirtrack_main/task/task_edit.html')
