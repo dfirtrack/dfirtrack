8750590dd606e6a39e88872a8051b97f49e47256
Mathias Stuhlmacher <mathias.stuhlmacher@gmx.de>
2020-10-24 21:52:18 +0200

    added missing test for artifact_exporter_spreadsheet_xls_config

--- dfirtrack_config/tests/artifact/test_artifact_exporter_spreadsheet_xls_config_views.py
+++ dfirtrack_config/tests/artifact/test_artifact_exporter_spreadsheet_xls_config_views.py
@@ -65,6 +65,23 @@ class ArtifactExporterSpreadsheetXlsConfigViewTestCase(TestCase):
         # compare
         self.assertRedirects(response, destination, status_code=301, target_status_code=200)
 
+    def test_artifact_exporter_spreadsheet_xls_config_post_redirect(self):
+        """ test view """
+
+        # login testuser
+        login = self.client.login(username='testuser_artifact_exporter_spreadsheet_xls_config', password='i3jLLnbrAEgel24sGs9i')
+        # get objects
+        artifactstatus_1 = Artifactstatus.objects.get(artifactstatus_name = 'Needs analysis').artifactstatus_id
+        artifactstatus_2 = Artifactstatus.objects.get(artifactstatus_name = 'Requested from customer').artifactstatus_id
+        # create post data
+        data_dict = {
+            'artifactlist_xls_choice_artifactstatus': [str(artifactstatus_1), str(artifactstatus_2)],
+        }
+        # get response
+        response = self.client.post('/config/artifact/exporter/spreadsheet/xls/', data_dict)
+        # compare
+        self.assertEqual(response.status_code, 200)
+
     def test_artifact_exporter_spreadsheet_xls_config_post_artifact_id_false(self):
         """ test view """
 
