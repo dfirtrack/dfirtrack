f897ea6c3e9840b9d06495b4cc987400696a7c02
Mathias Stuhlmacher <mathias.stuhlmacher@gmx.de>
2020-10-24 20:59:32 +0200

    added tests for artifact_exporter_spreadsheet_xls_config

--- dfirtrack_config/tests/artifact/test_artifact_exporter_spreadsheet_xls_config_views.py
+++ dfirtrack_config/tests/artifact/test_artifact_exporter_spreadsheet_xls_config_views.py
@@ -1,3 +1,5 @@
+from dfirtrack_artifacts.models import Artifactstatus
+from dfirtrack_config.models import ArtifactExporterSpreadsheetXlsConfigModel
 from django.contrib.auth.models import User
 from django.test import TestCase
 import urllib.parse
@@ -62,6 +64,45 @@ class ArtifactExporterSpreadsheetXlsConfigViewTestCase(TestCase):
         response = self.client.get('/config/artifact/exporter/spreadsheet/xls', follow=True)
         # compare
         self.assertRedirects(response, destination, status_code=301, target_status_code=200)
+
+    def test_artifact_exporter_spreadsheet_xls_config_post_artifact_id_false(self):
+        """ test view """
+
+        # login testuser
+        login = self.client.login(username='testuser_artifact_exporter_spreadsheet_xls_config', password='i3jLLnbrAEgel24sGs9i')
+        # get objects
+        artifactstatus_1 = Artifactstatus.objects.get(artifactstatus_name = 'Needs analysis').artifactstatus_id
+        artifactstatus_2 = Artifactstatus.objects.get(artifactstatus_name = 'Requested from customer').artifactstatus_id
+        # create post data
+        data_dict = {
+            'artifactlist_xls_choice_artifactstatus': [str(artifactstatus_1), str(artifactstatus_2)],
+        }
+        # get response
+        response = self.client.post('/config/artifact/exporter/spreadsheet/xls/', data_dict)
+        # get object
+        artifact_exporter_spreadsheet_xls_config_model = ArtifactExporterSpreadsheetXlsConfigModel.objects.get(artifact_exporter_spreadsheet_xls_config_name = 'ArtifactExporterSpreadsheetXlsConfig')
+        # compare
+        self.assertEqual(artifact_exporter_spreadsheet_xls_config_model.artifactlist_xls_artifact_id, False)
+
+    def test_artifact_exporter_spreadsheet_xls_config_post_artifact_id_true(self):
+        """ test view """
+
+        # login testuser
+        login = self.client.login(username='testuser_artifact_exporter_spreadsheet_xls_config', password='i3jLLnbrAEgel24sGs9i')
+        # get objects
+        artifactstatus_1 = Artifactstatus.objects.get(artifactstatus_name = 'Needs analysis').artifactstatus_id
+        artifactstatus_2 = Artifactstatus.objects.get(artifactstatus_name = 'Requested from customer').artifactstatus_id
+        # create post data
+        data_dict = {
+            'artifactlist_xls_choice_artifactstatus': [str(artifactstatus_1), str(artifactstatus_2)],
+            'artifactlist_xls_artifact_id': 'on',
+        }
+        # get response
+        response = self.client.post('/config/artifact/exporter/spreadsheet/xls/', data_dict)
+        # get object
+        artifact_exporter_spreadsheet_xls_config_model = ArtifactExporterSpreadsheetXlsConfigModel.objects.get(artifact_exporter_spreadsheet_xls_config_name = 'ArtifactExporterSpreadsheetXlsConfig')
+        # compare
+        self.assertEqual(artifact_exporter_spreadsheet_xls_config_model.artifactlist_xls_artifact_id, True)
 
     def test_artifact_exporter_spreadsheet_xls_config_post_invalid_reload(self):
         """ test view """
