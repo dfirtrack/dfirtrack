23b84c36583c52bff9e09019366d232dd9e17684
Mathias Stuhlmacher <mathias.stuhlmacher@gmx.de>
2020-10-30 23:03:20 +0100

    first draft of test_system_exporter_markdown

--- dfirtrack_main/exporter/markdown/read_or_create_mkdocs_yml.py
+++ dfirtrack_main/exporter/markdown/read_or_create_mkdocs_yml.py
@@ -16,7 +16,7 @@ def read_or_create_mkdocs_yml(request_user, mkdconfpath):
     except FileNotFoundError:
         # call logger
         warning_logger(request_user, " SYSTEM_EXPORTER_MARKDOWN no mkdocs.yml found")
-        # set empty dumm ydict for non-existing file to make code work as usual (just for the first execution because afterwards 'mkdocs.yml' will be there)
+        # set empty dummydict for non-existing file to make code work as usual (just for the first execution because afterwards 'mkdocs.yml' will be there)
         mkdconfdict = {'pages': [{'Systems': []}]}
 
     return mkdconfdict
