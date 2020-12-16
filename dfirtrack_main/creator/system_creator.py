0ab4fabf9cc235737326664213a4d50d9a2af2b6
Mathias Stuhlmacher <mathias.stuhlmacher@gmx.de>
2020-12-16 22:31:07 +0100

    added asynchronous messages for system creator

--- dfirtrack_main/creator/system_creator.py
+++ dfirtrack_main/creator/system_creator.py
@@ -1,8 +1,11 @@
+from django.contrib import messages
+from django.contrib.messages import constants
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import redirect, render
 from django.urls import reverse
 from django.utils import timezone
 from django_q.tasks import async_task
+from dfirtrack_main.async_messages import message_user
 from dfirtrack_main.forms import SystemCreatorForm
 from dfirtrack_main.logger.default_logger import debug_logger, error_logger, warning_logger
 from dfirtrack_main.models import System
@@ -13,16 +16,29 @@ def system_creator(request):
 
     # form was valid to post
     if request.method == "POST":
+
+        # get objects from request object
         request_post = request.POST
         request_user = request.user
+
+        # get time for messages
+        creator_time = timezone.now().strftime('[%Y-%m-%d %H:%M:%S]')
+
+        # create string for messages
+        creator_time_string = 'System creator ' + creator_time + ' '
+
+        # show immediate message for user
+        messages.success(request, creator_time_string + 'started')
 
         # call async function
         async_task(
             "dfirtrack_main.creator.system_creator.system_creator_async",
             request_post,
             request_user,
+            creator_time_string,
         )
 
+        # return directly to system list
         return redirect(reverse('system_list'))
 
     # show empty form
@@ -33,38 +49,70 @@ def system_creator(request):
         })
 
     # call logger
-    debug_logger(str(request.user), " SYSTEM_CREATOR_ENTERED")
+    debug_logger(str(request.user), ' SYSTEM_CREATOR_ENTERED')
+
     return render(request, 'dfirtrack_main/system/system_creator.html', {'form': form})
 
-def system_creator_async(request_post, request_user):
+def system_creator_async(request_post, request_user, creator_time_string):
     """ function to create many systems at once """
 
     # call logger
-    debug_logger(str(request_user), " SYSTEM_CREATOR_BEGIN")
+    debug_logger(str(request_user), ' SYSTEM_CREATOR_BEGIN')
 
     # exctract lines from systemlist (list results from request object via large text area)
     lines = request_post.get('systemlist').splitlines()
+
+    #  count lines (needed for messages)
+    number_of_lines = len(lines)
+
+    # set lines faulty counter (needed for messages)
+    lines_faulty_counter = 0
+
+    # set systems_created_counter (needed for messages)
+    systems_created_counter = 0
+
+    # set systems_skipped_counter (needed for messages)
+    systems_skipped_counter = 0
+
+    # create empty list (needed for messages)
+    skipped_systems = []
 
     # iterate over lines
     for line in lines:
 
         # skip emtpy lines
         if line == '':
-            warning_logger(str(request_user), " SYSTEM_CREATOR_ROW_EMPTY")
+            # autoincrement counter
+            lines_faulty_counter += 1
+            # call logger
+            warning_logger(str(request_user), ' SYSTEM_CREATOR_ROW_EMPTY')
             continue
 
         # check line for length of string
         if len(line) > 50:
-            warning_logger(str(request_user), " SYSTEM_CREATOR_LONG_STRING")
+            # autoincrement counter
+            lines_faulty_counter += 1
+            # call logger
+            warning_logger(str(request_user), ' SYSTEM_CREATOR_LONG_STRING')
             continue
 
         # check for existence of system
         system = System.objects.filter(system_name = line)
+
+        """ already existing system """
+
+        # in case of existing system
         if system.count() > 0:
+            # autoincrement counter
+            systems_skipped_counter += 1
+            # add system name to list of skipped systems
+            skipped_systems.append(line)
             # call logger
-            error_logger(str(request_user), " SYSTEM_CREATOR_SYSTEM_EXISTS " + "system_name:" + line)
+            error_logger(str(request_user), ' SYSTEM_CREATOR_SYSTEM_EXISTS ' + 'system_name:' + line)
             # leave this loop because system with this systemname already exists
             continue
+
+        """ new system """
 
         # create form with request data
         form = SystemCreatorForm(request_post)
@@ -89,8 +137,36 @@ def system_creator_async(request_post, request_user):
             # save manytomany
             form.save_m2m()
 
+            # autoincrement counter
+            systems_created_counter  += 1
+
             # call logger
             system.logger(str(request_user), ' SYSTEM_CREATOR_EXECUTED')
 
+    """ call final messages """
+
+    # finish message
+    message_user(request_user, creator_time_string + 'finished', constants.SUCCESS)
+
+    if systems_created_counter > 0:
+        if systems_created_counter  == 1:
+            message_user(request_user, str(systems_created_counter) + ' system was created.', constants.SUCCESS)
+        else:
+            message_user(request_user, str(systems_created_counter) + ' systems were created.', constants.SUCCESS)
+
+    if systems_skipped_counter > 0:
+        if systems_skipped_counter  == 1:
+            message_user(request_user, str(systems_skipped_counter) + ' system was skipped. ' + str(skipped_systems), constants.ERROR)
+        else:
+            message_user(request_user, str(systems_skipped_counter) + ' systems were skipped. ' + str(skipped_systems), constants.ERROR)
+
+    if lines_faulty_counter > 0:
+        if lines_faulty_counter  == 1:
+            message_user(request_user, str(lines_faulty_counter) + ' line out of ' + str(number_of_lines) + ' lines was faulty (see log file for details).', constants.WARNING)
+        else:
+            message_user(request_user, str(lines_faulty_counter) + ' lines out of ' + str(number_of_lines) + ' lines were faulty (see log file for details).', constants.WARNING)
+
+    # TODO: add logger (status)
+
     # call logger
-    debug_logger(str(request_user), " SYSTEM_CREATOR_END")
+    debug_logger(str(request_user), ' SYSTEM_CREATOR_END')
