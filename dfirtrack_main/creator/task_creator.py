9e7386199fe5bcc9cff8e8fb6c4daafb2f858bb7
Mathias Stuhlmacher <mathias.stuhlmacher@gmx.de>
2020-12-16 23:54:35 +0100

    added asynchronous messages for task creator

--- dfirtrack_main/creator/task_creator.py
+++ dfirtrack_main/creator/task_creator.py
@@ -1,11 +1,17 @@
+from django.contrib import messages
+from django.contrib.messages import constants
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import redirect, render
 from django.urls import reverse
 from django.utils import timezone
 from django_q.tasks import async_task
+from dfirtrack_main.async_messages import message_user
 from dfirtrack_main.forms import TaskCreatorForm
-from dfirtrack_main.logger.default_logger import debug_logger
+from dfirtrack_main.logger.default_logger import debug_logger, info_logger
 from dfirtrack_main.models import System, Taskname, Taskstatus
+
+
+# TODO: test messages
 
 @login_required(login_url="/login")
 def task_creator(request):
@@ -13,16 +19,29 @@ def task_creator(request):
 
     # form was valid to post
     if request.method == 'POST':
+
+        # get objects from request object
         request_post = request.POST
         request_user = request.user
+
+        # get time for messages
+        creator_time = timezone.now().strftime('[%Y-%m-%d %H:%M:%S]')
+
+        # create string for messages
+        creator_time_string = 'Task creator ' + creator_time + ' '
+
+        # show immediate message for user
+        messages.success(request, creator_time_string + 'started')
 
         # call async function
         async_task(
             "dfirtrack_main.creator.task_creator.task_creator_async",
             request_post,
             request_user,
+            creator_time_string,
         )
 
+        # return directly to system list
         return redirect(reverse('task_list'))
 
     # show empty form
@@ -34,9 +53,10 @@ def task_creator(request):
 
         # call logger
         debug_logger(str(request.user), " TASK_CREATOR_ENTERED")
+
     return render(request, 'dfirtrack_main/task/task_creator.html', {'form': form})
 
-def task_creator_async(request_post, request_user):
+def task_creator_async(request_post, request_user, creator_time_string):
     """ function to create many tasks for many systems at once """
 
     # call logger
@@ -47,6 +67,9 @@ def task_creator_async(request_post, request_user):
 
     # extract systems (list results from request object via multiple choice field)
     systems = request_post.getlist('system')
+
+    # set tasks_created_counter (needed for messages)
+    tasks_created_counter = 0
 
     # iterate over tasknames
     for taskname in tasknames:
@@ -88,8 +111,25 @@ def task_creator_async(request_post, request_user):
                 # save manytomany
                 form.save_m2m()
 
+                # autoincrement counter
+                tasks_created_counter  += 1
+
                 # call logger
                 task.logger( str(request_user), " TASK_CREATOR_EXECUTED")
+
+    """ call final messages """
+
+    # finish message
+    message_user(request_user, creator_time_string + 'finished', constants.SUCCESS)
+
+    if tasks_created_counter > 0:
+        if tasks_created_counter  == 1:
+            message_user(request_user, str(tasks_created_counter) + ' task was created.', constants.SUCCESS)
+        else:
+            message_user(request_user, str(tasks_created_counter) + ' tasks were created.', constants.SUCCESS)
+
+    # call logger
+    info_logger(str(request_user), ' TASK_CREATOR_STATUS ' + 'created:' + str(tasks_created_counter))
 
     # call logger
     debug_logger(str(request_user), " TASK_CREATOR_END")
