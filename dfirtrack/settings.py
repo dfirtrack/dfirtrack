a74c979d68642834ef1b5ac83b5a4574faac72f9
Alexander Gödeke <alexander.goedeke@rvwaltrop.de>
2020-12-20 14:35:03 +0100

    Add support of environment variables for the docker container.

--- dfirtrack/settings.py
+++ dfirtrack/settings.py
@@ -9,7 +9,7 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'CHANGEME'
+SECRET_KEY = os.getenv('SECRET_KEY', 'CHANGEME')
 
 # Application definition
 
