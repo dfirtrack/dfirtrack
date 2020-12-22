12da44691de6863472c625bab7809d33fd1251b2
Mathias Stuhlmacher <mathias.stuhlmacher@gmx.de>
2020-12-22 12:28:19 +0100

    removed deprecated entry_importer_file_markdown

--- dfirtrack_main/urls.py
+++ dfirtrack_main/urls.py
@@ -5,9 +5,9 @@ from dfirtrack_main.views import analysisstatus_views, analystmemo_views, case_v
 from dfirtrack_main.creator import system_creator, tag_creator, task_creator
 from dfirtrack_main.exporter.spreadsheet import csv as spreadsheet_csv
 from dfirtrack_main.exporter.spreadsheet import xls
-from dfirtrack_main.exporter.markdown import markdown as markdown_exporter
+from dfirtrack_main.exporter.markdown import markdown
 from dfirtrack_main.importer.file import csv_config_based, csv_form_based
-from dfirtrack_main.importer.file import filesystem, markdown
+from dfirtrack_main.importer.file import filesystem
 from dfirtrack_main.modificator import system_modificator
 
 urlpatterns = [
@@ -64,8 +64,6 @@ urlpatterns = [
     path(r'entry/add/', entry_views.EntryCreate.as_view(), name='entry_create'),
     path(r'entry/<int:pk>/edit/', entry_views.EntryUpdate.as_view(), name='entry_update'),
 
-    path(r'entry/importer/file/markdown/entry/', markdown.entry, name='entry_importer_file_markdown'),
-
     path(r'headline/', headline_views.HeadlineList.as_view(), name='headline_list'),
     path(r'headline/<int:pk>/', headline_views.HeadlineDetail.as_view(), name='headline_detail'),
     path(r'headline/add/', headline_views.HeadlineCreate.as_view(), name='headline_create'),
@@ -121,7 +119,7 @@ urlpatterns = [
     path(r'system/<int:pk>/edit/', system_views.SystemUpdate.as_view(), name='system_update'),
 
     path(r'system/creator/', system_creator.system_creator, name='system_creator'),
-    path(r'system/exporter/markdown/system/', markdown_exporter.system, name='system_exporter_markdown'),
+    path(r'system/exporter/markdown/system/', markdown.system, name='system_exporter_markdown'),
     path(r'system/exporter/spreadsheet/csv/system/', spreadsheet_csv.system, name='system_exporter_spreadsheet_csv'),
     path(r'system/exporter/spreadsheet/xls/system/', xls.system, name='system_exporter_spreadsheet_xls'),
     path(r'system/importer/file/csv/configbased/', csv_config_based.system, name='system_importer_file_csv_config_based'),
