ba649021df717cedcabdf857a03bd5c479bbed3a
Fabian Franz <fabfaeb@gmail.com>
2021-01-17 09:33:26 +0100

    removed empty line

--- dfirtrack_main/static/dfirtrack_main/dfirtrack/datatables_dfirtrack.js
+++ dfirtrack_main/static/dfirtrack_main/dfirtrack/datatables_dfirtrack.js
@@ -14,7 +14,6 @@ $(document).ready( function () {
         "lengthMenu": [[10, 25, 50, -1], [10, 25, 50, "All"]],
         "order": [[ 5, "desc" ]]
     } );
-
     $('#table_system').DataTable( {
         "pageLength": 25,
         "lengthMenu": [[10, 25, 50, -1], [10, 25, 50, "All"]],
