f012afd6262b7545c4ad846e8791e485a6a6ade6
alexgoedeke <alexgoedeke@users.noreply.github.com>
2021-02-17 12:24:31 +0100

    add automatic superuser creation

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -22,8 +22,14 @@ service nginx start
 $APPDIR/manage.py migrate
 $APPDIR/manage.py createcachetable
 $APPDIR/manage.py qcluster &
+
+if [ $DJANGO_SUPERUSER_USERNAME ] && [ $DJANGO_SUPERUSER_EMAIL ] && [ $DJANGO_SUPERUSER_PASSWORD ]; then
+    echo "[+] Creating super user $DJANGO_SUPERUSER_USERNAME"
+    $APPDIR/manage.py createsuperuser --noinput
+fi
+
 gunicorn --log-file=/var/log/gunicorn.log --workers 4 --bind localhost:5000 dfirtrack.wsgi &
 sleep 10
 echo "Container started"
 echo "!!!! You may run docker/setup_admin.sh from the host system to create a new superuser !!!!"
-sleep infinity 
+sleep infinity
