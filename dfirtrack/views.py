c16b0821ee15b172cdfc41d8b07b6642ef53d85d
Mathias Stuhlmacher <mathias.stuhlmacher@gmx.de>
2021-04-19 21:48:36 +0200

    moved url switching for main overview to code

--- dfirtrack/views.py
+++ dfirtrack/views.py
@@ -1,4 +1,36 @@
-from django.shortcuts import redirect, render
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import redirect
+from django.urls import reverse
+from dfirtrack import settings
+from dfirtrack_config.models import MainConfigModel
 
 def login_redirect(request):
+    """ redirect to login """
+
     return redirect('/login')
+
+@login_required
+def main_overview_redirect(request):
+    """ redirect main overview according to config """
+
+    # get config
+    model = MainConfigModel.objects.get(main_config_name='MainConfig')
+
+    # system
+    if model.main_overview == 'main_overview_system':
+        return redirect(reverse('system_list'))
+    # artifact
+    elif model.main_overview == 'main_overview_artifact' and 'dfirtrack_artifacts' in settings.INSTALLED_APPS:
+        return redirect(reverse('artifact_list'))
+    # case
+    elif model.main_overview == 'main_overview_case':
+        return redirect(reverse('case_list'))
+    # tag
+    elif model.main_overview == 'main_overview_tag':
+        return redirect(reverse('tag_list'))
+    # task
+    elif model.main_overview == 'main_overview_task':
+        return redirect(reverse('task_list'))
+    # catch-up pattern
+    else:
+        return redirect(reverse('system_list'))
