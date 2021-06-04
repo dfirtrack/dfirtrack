c135c32f799f284ad1ed0b5cf5ed92392302e142
Mathias Stuhlmacher <mathias.stuhlmacher@gmx.de>
2021-06-04 23:09:02 +0200

    removed legacy code from templatetags

--- dfirtrack_main/templatetags/dfirtrack_main_tags.py
+++ dfirtrack_main/templatetags/dfirtrack_main_tags.py
@@ -14,12 +14,8 @@ def dfirtrack_branch():
     repo = Repo(getcwd())
     try:
         return repo.active_branch
-    except TypeError:
+    except TypeError:               # coverage: ignore branch
         try:
             return repo.git.describe('--tags')
         except GitCommandError:
             return repo.git.describe('--all')
-
-@register.simple_tag
-def github_ci():
-    return 'CI' in environ
