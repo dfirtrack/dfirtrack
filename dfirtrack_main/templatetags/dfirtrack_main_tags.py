46cc028b6983ab28b6e1658d9f7025ebd626f48e
Mathias Stuhlmacher <mathias.stuhlmacher@gmx.de>
2021-06-08 19:12:41 +0200

    increased version number to 1.3.0

--- dfirtrack_main/templatetags/dfirtrack_main_tags.py
+++ dfirtrack_main/templatetags/dfirtrack_main_tags.py
@@ -6,7 +6,7 @@ register = template.Library()
 
 @register.simple_tag
 def dfirtrack_version():
-    versionnumber = 'v1.2.1'
+    versionnumber = 'v1.3.0'
     return versionnumber
 
 @register.simple_tag
