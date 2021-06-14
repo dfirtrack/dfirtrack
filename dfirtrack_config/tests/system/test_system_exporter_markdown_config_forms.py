1f93120423a686cc29033f5bc57f8cc687168e48
Mathias Stuhlmacher <mathias.stuhlmacher@gmx.de>
2021-06-14 22:25:47 +0200

    finished config tests

--- dfirtrack_config/tests/system/test_system_exporter_markdown_config_forms.py
+++ dfirtrack_config/tests/system/test_system_exporter_markdown_config_forms.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from dfirtrack_config.forms import SystemExporterMarkdownConfigForm
 
+
 class SystemExporterMarkdownConfigFormTestCase(TestCase):
     """ system exporter markdown config form tests """
 
@@ -20,30 +21,12 @@ class SystemExporterMarkdownConfigFormTestCase(TestCase):
         # compare
         self.assertEqual(form.fields['markdown_sorting'].label, 'Choose sorting for system markdown export')
 
-    def test_system_exporter_markdown_config_form_empty(self):
-        """ test minimum form requirements / INVALID """
-
-        # get object
-        form = SystemExporterMarkdownConfigForm(data = {})
-        # compare
-        self.assertFalse(form.is_valid())
-
     def test_system_exporter_markdown_config_markdown_path_form_filled(self):
         """ test minimum form requirements / INVALID """
 
         # get object
         form = SystemExporterMarkdownConfigForm(data = {
             'markdown_path': '/tmp',
-        })
-        # compare
-        self.assertFalse(form.is_valid())
-
-    def test_system_exporter_markdown_config_markdown_sorting_form_filled(self):
-        """ test minimum form requirements / INVALID """
-
-        # get object
-        form = SystemExporterMarkdownConfigForm(data = {
-            'markdown_sorting': '/tmp',
         })
         # compare
         self.assertFalse(form.is_valid())
@@ -58,3 +41,27 @@ class SystemExporterMarkdownConfigFormTestCase(TestCase):
         })
         # compare
         self.assertTrue(form.is_valid())
+
+    def test_system_exporter_markdown_config_markdown_path_not_existent(self):
+        """ test field validation """
+
+        # get object
+        form = SystemExporterMarkdownConfigForm(data = {
+            'markdown_path': '/foobar',
+            'markdown_sorting': 'dom',
+        })
+        # compare
+        self.assertFalse(form.is_valid())
+        self.assertEqual(form.errors['markdown_path'], ['Markdown path does not exist.'])
+
+    def test_system_exporter_markdown_config_markdown_path_no_write_permission(self):
+        """ test field validation """
+
+        # get object
+        form = SystemExporterMarkdownConfigForm(data = {
+            'markdown_path': '/root',
+            'markdown_sorting': 'dom',
+        })
+        # compare
+        self.assertFalse(form.is_valid())
+        self.assertEqual(form.errors['markdown_path'], ['No write permission for markdown path.'])
