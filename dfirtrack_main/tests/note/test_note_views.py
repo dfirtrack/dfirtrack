57d366c39b22d89642ce6c9ba91a09e34aae3b76
Mathias Stuhlmacher <mathias.stuhlmacher@gmx.de>
2021-06-20 17:56:16 +0200

    added note view tests

---
+++ dfirtrack_main/tests/note/test_note_views.py
@@ -0,0 +1,360 @@
+from django.contrib.auth.models import User
+from django.test import TestCase
+from dfirtrack_main.models import Note
+from dfirtrack_main.models import Notestatus
+import urllib.parse
+
+
+class NoteViewTestCase(TestCase):
+    """ note view tests """
+
+    @classmethod
+    def setUpTestData(cls):
+
+        # create user
+        test_user = User.objects.create_user(username='testuser_note', password='oh8Szsuk8BpbEJ1RRL21')
+
+        # create objects
+        Notestatus.objects.create(notestatus_name='notestatus_1')
+
+        # create object
+        Note.objects.create(
+            note_title = 'note_1',
+            note_content = 'lorem ipsum',
+            note_created_by_user_id = test_user,
+            note_modified_by_user_id = test_user,
+        )
+
+    def test_note_list_not_logged_in(self):
+        """ test list view """
+
+        # create url
+        destination = '/login/?next=' + urllib.parse.quote('/note/', safe='')
+        # get response
+        response = self.client.get('/note/', follow=True)
+        # compare
+        self.assertRedirects(response, destination, status_code=302, target_status_code=200)
+
+    def test_note_list_logged_in(self):
+        """ test list view """
+
+        # login testuser
+        self.client.login(username='testuser_note', password='oh8Szsuk8BpbEJ1RRL21')
+        # get response
+        response = self.client.get('/note/')
+        # compare
+        self.assertEqual(response.status_code, 200)
+
+    def test_note_list_template(self):
+        """ test list view """
+
+        # login testuser
+        self.client.login(username='testuser_note', password='oh8Szsuk8BpbEJ1RRL21')
+        # get response
+        response = self.client.get('/note/')
+        # compare
+        self.assertTemplateUsed(response, 'dfirtrack_main/note/note_list.html')
+
+    def test_note_list_get_user_context(self):
+        """ test list view """
+
+        # login testuser
+        self.client.login(username='testuser_note', password='oh8Szsuk8BpbEJ1RRL21')
+        # get response
+        response = self.client.get('/note/')
+        # compare
+        self.assertEqual(str(response.context['user']), 'testuser_note')
+
+    def test_note_list_redirect(self):
+        """ test list view """
+
+        # login testuser
+        self.client.login(username='testuser_note', password='oh8Szsuk8BpbEJ1RRL21')
+        # create url
+        destination = urllib.parse.quote('/note/', safe='/')
+        # get response
+        response = self.client.get('/note', follow=True)
+        # compare
+        self.assertRedirects(response, destination, status_code=301, target_status_code=200)
+
+    def test_note_detail_not_logged_in(self):
+        """ test detail view """
+
+        # get object
+        note_1 = Note.objects.get(note_title='note_1')
+        # create url
+        destination = '/login/?next=' + urllib.parse.quote('/note/' + str(note_1.note_id) + '/', safe='')
+        # get response
+        response = self.client.get('/note/' + str(note_1.note_id) + '/', follow=True)
+        # compare
+        self.assertRedirects(response, destination, status_code=302, target_status_code=200)
+
+    def test_note_detail_logged_in(self):
+        """ test detail view """
+
+        # get object
+        note_1 = Note.objects.get(note_title='note_1')
+        # login testuser
+        self.client.login(username='testuser_note', password='oh8Szsuk8BpbEJ1RRL21')
+        # get response
+        response = self.client.get('/note/' + str(note_1.note_id) + '/')
+        # compare
+        self.assertEqual(response.status_code, 200)
+
+    def test_note_detail_template(self):
+        """ test detail view """
+
+        # get object
+        note_1 = Note.objects.get(note_title='note_1')
+        # login testuser
+        self.client.login(username='testuser_note', password='oh8Szsuk8BpbEJ1RRL21')
+        # get response
+        response = self.client.get('/note/' + str(note_1.note_id) + '/')
+        # compare
+        self.assertTemplateUsed(response, 'dfirtrack_main/note/note_detail.html')
+
+    def test_note_detail_get_user_context(self):
+        """ test detail view """
+
+        # get object
+        note_1 = Note.objects.get(note_title='note_1')
+        # login testuser
+        self.client.login(username='testuser_note', password='oh8Szsuk8BpbEJ1RRL21')
+        # get response
+        response = self.client.get('/note/' + str(note_1.note_id) + '/')
+        # compare
+        self.assertEqual(str(response.context['user']), 'testuser_note')
+
+    def test_note_detail_redirect(self):
+        """ test detail view """
+
+        # get object
+        note_1 = Note.objects.get(note_title='note_1')
+        # login testuser
+        self.client.login(username='testuser_note', password='oh8Szsuk8BpbEJ1RRL21')
+        # create url
+        destination = urllib.parse.quote('/note/' + str(note_1.note_id) + '/', safe='/')
+        # get response
+        response = self.client.get('/note/' + str(note_1.note_id), follow=True)
+        # compare
+        self.assertRedirects(response, destination, status_code=301, target_status_code=200)
+
+    def test_note_add_not_logged_in(self):
+        """ test add view """
+
+        # create url
+        destination = '/login/?next=' + urllib.parse.quote('/note/add/', safe='')
+        # get response
+        response = self.client.get('/note/add/', follow=True)
+        # compare
+        self.assertRedirects(response, destination, status_code=302, target_status_code=200)
+
+    def test_note_add_logged_in(self):
+        """ test add view """
+
+        # login testuser
+        self.client.login(username='testuser_note', password='oh8Szsuk8BpbEJ1RRL21')
+        # get response
+        response = self.client.get('/note/add/')
+        # compare
+        self.assertEqual(response.status_code, 200)
+
+    def test_note_add_template(self):
+        """ test add view """
+
+        # login testuser
+        self.client.login(username='testuser_note', password='oh8Szsuk8BpbEJ1RRL21')
+        # get response
+        response = self.client.get('/note/add/')
+        # compare
+        self.assertTemplateUsed(response, 'dfirtrack_main/note/note_generic_form.html')
+
+    def test_note_add_get_user_context(self):
+        """ test add view """
+
+        # login testuser
+        self.client.login(username='testuser_note', password='oh8Szsuk8BpbEJ1RRL21')
+        # get response
+        response = self.client.get('/note/add/')
+        # compare
+        self.assertEqual(str(response.context['user']), 'testuser_note')
+
+    def test_note_add_redirect(self):
+        """ test add view """
+
+        # login testuser
+        self.client.login(username='testuser_note', password='oh8Szsuk8BpbEJ1RRL21')
+        # create url
+        destination = urllib.parse.quote('/note/add/', safe='/')
+        # get response
+        response = self.client.get('/note/add', follow=True)
+        # compare
+        self.assertRedirects(response, destination, status_code=301, target_status_code=200)
+
+    def test_note_add_post_redirect(self):
+        """ test add view """
+
+        # login testuser
+        self.client.login(username='testuser_note', password='oh8Szsuk8BpbEJ1RRL21')
+        # get objects
+        notestatus_id = Notestatus.objects.get(notestatus_name = 'notestatus_1').notestatus_id
+        # create post data
+        data_dict = {
+            'note_title': 'note_add_post_test',
+            'note_content': 'lorem ipsum',
+            'notestatus': notestatus_id,
+        }
+        # get response
+        response = self.client.post('/note/add/', data_dict)
+        # get object
+        note_add_post_test = Note.objects.get(note_title='note_add_post_test')
+        # create url
+        destination = urllib.parse.quote('/note/' + str(note_add_post_test.note_id) + '/', safe='/')
+        # compare
+        self.assertRedirects(response, destination, status_code=302, target_status_code=200)
+
+    def test_note_add_post_invalid_reload(self):
+        """ test add view """
+
+        # login testuser
+        self.client.login(username='testuser_note', password='oh8Szsuk8BpbEJ1RRL21')
+        # create post data
+        data_dict = {}
+        # get response
+        response = self.client.post('/note/add/', data_dict)
+        # compare
+        self.assertEqual(response.status_code, 200)
+
+    def test_note_add_post_invalid_template(self):
+        """ test add view """
+
+        # login testuser
+        self.client.login(username='testuser_note', password='oh8Szsuk8BpbEJ1RRL21')
+        # create post data
+        data_dict = {}
+        # get response
+        response = self.client.post('/note/add/', data_dict)
+        # compare
+        self.assertTemplateUsed(response, 'dfirtrack_main/note/note_generic_form.html')
+
+    def test_note_edit_not_logged_in(self):
+        """ test edit view """
+
+        # get object
+        note_1 = Note.objects.get(note_title='note_1')
+        # create url
+        destination = '/login/?next=' + urllib.parse.quote('/note/' + str(note_1.note_id) + '/edit/', safe='')
+        # get response
+        response = self.client.get('/note/' + str(note_1.note_id) + '/edit/', follow=True)
+        # compare
+        self.assertRedirects(response, destination, status_code=302, target_status_code=200)
+
+    def test_note_edit_logged_in(self):
+        """ test edit view """
+
+        # get object
+        note_1 = Note.objects.get(note_title='note_1')
+        # login testuser
+        self.client.login(username='testuser_note', password='oh8Szsuk8BpbEJ1RRL21')
+        # get response
+        response = self.client.get('/note/' + str(note_1.note_id) + '/edit/')
+        # compare
+        self.assertEqual(response.status_code, 200)
+
+    def test_note_edit_template(self):
+        """ test edit view """
+
+        # get object
+        note_1 = Note.objects.get(note_title='note_1')
+        # login testuser
+        self.client.login(username='testuser_note', password='oh8Szsuk8BpbEJ1RRL21')
+        # get response
+        response = self.client.get('/note/' + str(note_1.note_id) + '/edit/')
+        # compare
+        self.assertTemplateUsed(response, 'dfirtrack_main/note/note_generic_form.html')
+
+    def test_note_edit_get_user_context(self):
+        """ test edit view """
+
+        # get object
+        note_1 = Note.objects.get(note_title='note_1')
+        # login testuser
+        self.client.login(username='testuser_note', password='oh8Szsuk8BpbEJ1RRL21')
+        # get response
+        response = self.client.get('/note/' + str(note_1.note_id) + '/edit/')
+        # compare
+        self.assertEqual(str(response.context['user']), 'testuser_note')
+
+    def test_note_edit_redirect(self):
+        """ test edit view """
+
+        # get object
+        note_1 = Note.objects.get(note_title='note_1')
+        # login testuser
+        self.client.login(username='testuser_note', password='oh8Szsuk8BpbEJ1RRL21')
+        # create url
+        destination = urllib.parse.quote('/note/' + str(note_1.note_id) + '/edit/', safe='/')
+        # get response
+        response = self.client.get('/note/' + str(note_1.note_id) + '/edit', follow=True)
+        # compare
+        self.assertRedirects(response, destination, status_code=301, target_status_code=200)
+
+    def test_note_edit_post_redirect(self):
+        """ test edit view """
+
+        # login testuser
+        self.client.login(username='testuser_note', password='oh8Szsuk8BpbEJ1RRL21')
+        # get user
+        test_user = User.objects.get(username='testuser_note')
+        # get objects
+        notestatus_1 = Notestatus.objects.get(notestatus_name = 'notestatus_1')
+        # create object
+        note_1 = Note.objects.create(
+            note_title = 'note_edit_post_test_1',
+            note_content = 'lorem ipsum',
+            notestatus = notestatus_1,
+            note_created_by_user_id = test_user,
+            note_modified_by_user_id = test_user,
+        )
+        # create post data
+        data_dict = {
+            'note_content': 'note_edit_post_test_2',
+            'note_title': 'lorem ipsum',
+            'notestatus': notestatus_1.notestatus_id,
+        }
+        # get response
+        response = self.client.post('/note/' + str(note_1.note_id) + '/edit/', data_dict)
+        # create url
+        destination = urllib.parse.quote('/note/' + str(note_1.note_id) + '/', safe='/')
+        # get object
+        note_edit_post_test_2 = Note.objects.get(note_title='note_edit_post_test_2')
+        # compare
+        self.assertRedirects(response, destination, status_code=302, target_status_code=200)
+
+    def test_note_edit_post_invalid_reload(self):
+        """ test edit view """
+
+        # login testuser
+        self.client.login(username='testuser_note', password='oh8Szsuk8BpbEJ1RRL21')
+        # get object
+        note_id = Note.objects.get(note_title='note_1').note_id
+        # create post data
+        data_dict = {}
+        # get response
+        response = self.client.post('/note/' + str(note_id) + '/edit/', data_dict)
+        # compare
+        self.assertEqual(response.status_code, 200)
+
+    def test_note_edit_post_invalid_template(self):
+        """ test edit view """
+
+        # login testuser
+        self.client.login(username='testuser_note', password='oh8Szsuk8BpbEJ1RRL21')
+        # get object
+        note_id = Note.objects.get(note_title='note_1').note_id
+        # create post data
+        data_dict = {}
+        # get response
+        response = self.client.post('/note/' + str(note_id) + '/edit/', data_dict)
+        # compare
+        self.assertTemplateUsed(response, 'dfirtrack_main/note/note_generic_form.html')
