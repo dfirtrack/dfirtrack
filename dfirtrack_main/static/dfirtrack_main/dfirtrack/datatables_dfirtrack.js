2f92065e47d46175475ffcec30e734a651ac582f
Mathias Stuhlmacher <mathias.stuhlmacher@gmx.de>
2021-06-21 12:44:11 +0200

    moved reportitem datatable to include and added reportitem stuff to notestatus

--- dfirtrack_main/static/dfirtrack_main/dfirtrack/datatables_dfirtrack.js
+++ dfirtrack_main/static/dfirtrack_main/dfirtrack/datatables_dfirtrack.js
@@ -84,7 +84,7 @@ $(document).ready( function () {
         "lengthMenu": [[10, 25, 50, -1], [10, 25, 50, "All"]],
         "order": [[ 1, "asc" ]]
     } );
-    $('#table_notes').DataTable( {
+    $('#table_note').DataTable( {
         "pageLength": 25,
         "lengthMenu": [[10, 25, 50, -1], [10, 25, 50, "All"]],
         "order": [[ 0, "asc" ]]
