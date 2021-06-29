c9acc3c1ddf1dcf733bcc4d4e450b40f9d66437f
Mathias Stuhlmacher <mathias.stuhlmacher@gmx.de>
2021-06-29 09:21:13 +0200

    removed branch from maintemplate to avoid deployment errors

--- dfirtrack_main/templatetags/dfirtrack_main_tags.py
+++ dfirtrack_main/templatetags/dfirtrack_main_tags.py
@@ -1,6 +1,4 @@
 from django import template
-from git import Repo, GitCommandError
-from os import getcwd
 
 register = template.Library()
 
@@ -8,14 +6,3 @@ register = template.Library()
 def dfirtrack_version():
     versionnumber = 'v1.5.5'
     return versionnumber
-
-@register.simple_tag
-def dfirtrack_branch():
-    repo = Repo(getcwd())
-    try:
-        return repo.active_branch
-    except TypeError:               # coverage: ignore branch
-        try:
-            return repo.git.describe('--tags')
-        except GitCommandError:
-            return repo.git.describe('--all')
