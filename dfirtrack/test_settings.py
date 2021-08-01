192474a9828f006fee98ca0dcbed749c9a7385af
Christian Clauss <cclauss@me.com>
2021-08-01 14:28:21 +0200

    Update test_settings.py

--- dfirtrack/test_settings.py
+++ dfirtrack/test_settings.py
@@ -5,7 +5,7 @@ Test settings for DFIRTrack project.
 import logging
 import sys
 
-from dfirtrack.settings import *
+from dfirtrack.settings import Q_CLUSTER
 
 # disable logging while testing (don't want the log file to be cluttered with test log entries, also don't want to see django-q logs while testing)
 if len(sys.argv) > 1 and sys.argv[1] == 'test':
