0df1fc24d24a13205e2a2c5648d536812f7a42fe
Mathias Stuhlmacher <mathias.stuhlmacher@gmx.de>
2021-09-26 23:52:04 +0200

    debugging isort 2

--- dfirtrack_artifacts/creator/artifact_creator.py
+++ dfirtrack_artifacts/creator/artifact_creator.py
@@ -1,14 +1,15 @@
-from dfirtrack_artifacts.forms import ArtifactCreatorForm
-from dfirtrack_artifacts.models import Artifactpriority, Artifactstatus, Artifacttype
-from dfirtrack_main.async_messages import message_user
-from dfirtrack_main.logger.default_logger import debug_logger, info_logger
-from dfirtrack_main.models import System
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.contrib.messages import constants
 from django.shortcuts import redirect, render
 from django.urls import reverse
 from django_q.tasks import async_task
+
+from dfirtrack_artifacts.forms import ArtifactCreatorForm
+from dfirtrack_artifacts.models import Artifactpriority, Artifactstatus, Artifacttype
+from dfirtrack_main.async_messages import message_user
+from dfirtrack_main.logger.default_logger import debug_logger, info_logger
+from dfirtrack_main.models import System
 
 
 @login_required(login_url="/login")
