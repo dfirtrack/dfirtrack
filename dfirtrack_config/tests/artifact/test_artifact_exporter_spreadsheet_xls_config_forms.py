e364486c58601a3411ae75a06e321392a79b9467
Mathias Stuhlmacher <mathias.stuhlmacher@gmx.de>
2022-02-13 17:22:31 +0100

    updated artifact exporter config form test

--- dfirtrack_config/tests/artifact/test_artifact_exporter_spreadsheet_xls_config_forms.py
+++ dfirtrack_config/tests/artifact/test_artifact_exporter_spreadsheet_xls_config_forms.py
@@ -16,7 +16,7 @@ class ArtifactExporterSpreadsheetXlsConfigFormTestCase(TestCase):
             artifactstatus_slug='artifactstatus_1',
         )
 
-    def test_artifact_exporter_spreadsheet_xls_config_artifactlist_xls_choice_artifactstatus_form_label(
+    def test_artifact_exporter_spreadsheet_xls_config_form_labels(
         self,
     ):
         """test form label"""
@@ -28,229 +28,88 @@ class ArtifactExporterSpreadsheetXlsConfigFormTestCase(TestCase):
             form.fields['artifactlist_xls_choice_artifactstatus'].label,
             'Export only artifacts with this artifactstatus',
         )
-
-    def test_artifact_exporter_spreadsheet_xls_config_artifactlist_xls_artifact_id_form_label(
-        self,
-    ):
-        """test form label"""
-
-        # get object
-        form = ArtifactExporterSpreadsheetXlsConfigForm()
-        # compare
         self.assertEqual(
             form.fields['artifactlist_xls_artifact_id'].label, 'Export artifact ID'
         )
-
-    def test_artifact_exporter_spreadsheet_xls_config_artifactlist_xls_system_id_form_label(
-        self,
-    ):
-        """test form label"""
-
-        # get object
-        form = ArtifactExporterSpreadsheetXlsConfigForm()
-        # compare
-        self.assertEqual(
-            form.fields['artifactlist_xls_system_id'].label, 'Export system ID'
-        )
-
-    def test_artifact_exporter_spreadsheet_xls_config_artifactlist_xls_system_name_form_label(
-        self,
-    ):
-        """test form label"""
-
-        # get object
-        form = ArtifactExporterSpreadsheetXlsConfigForm()
-        # compare
-        self.assertEqual(
-            form.fields['artifactlist_xls_system_name'].label, 'Export system name'
-        )
-
-    def test_artifact_exporter_spreadsheet_xls_config_artifactlist_xls_artifactstatus_form_label(
-        self,
-    ):
-        """test form label"""
-
-        # get object
-        form = ArtifactExporterSpreadsheetXlsConfigForm()
-        # compare
-        self.assertEqual(
-            form.fields['artifactlist_xls_artifactstatus'].label,
-            'Export artifactstatus',
-        )
-
-    def test_artifact_exporter_spreadsheet_xls_config_artifactlist_xls_artifactpriority_form_label(
-        self,
-    ):
-        """test form label"""
-
-        # get object
-        form = ArtifactExporterSpreadsheetXlsConfigForm()
-        # compare
         self.assertEqual(
             form.fields['artifactlist_xls_artifactpriority'].label,
             'Export artifactpriority',
         )
-
-    def test_artifact_exporter_spreadsheet_xls_config_artifactlist_xls_artifacttype_form_label(
-        self,
-    ):
-        """test form label"""
-
-        # get object
-        form = ArtifactExporterSpreadsheetXlsConfigForm()
-        # compare
+        self.assertEqual(
+            form.fields['artifactlist_xls_artifactstatus'].label,
+            'Export artifactstatus',
+        )
         self.assertEqual(
             form.fields['artifactlist_xls_artifacttype'].label, 'Export artifacttype'
         )
-
-    def test_artifact_exporter_spreadsheet_xls_config_artifactlist_xls_artifact_source_path_form_label(
-        self,
-    ):
-        """test form label"""
-
-        # get object
-        form = ArtifactExporterSpreadsheetXlsConfigForm()
-        # compare
         self.assertEqual(
-            form.fields['artifactlist_xls_artifact_source_path'].label,
-            'Export source path',
+            form.fields['artifactlist_xls_case_id'].label, 'Export case ID'
         )
-
-    def test_artifact_exporter_spreadsheet_xls_config_artifactlist_xls_artifact_storage_path_form_label(
-        self,
-    ):
-        """test form label"""
-
-        # get object
-        form = ArtifactExporterSpreadsheetXlsConfigForm()
-        # compare
         self.assertEqual(
-            form.fields['artifactlist_xls_artifact_storage_path'].label,
-            'Export storage path',
+            form.fields['artifactlist_xls_case_name'].label, 'Export case name'
         )
-
-    def test_artifact_exporter_spreadsheet_xls_config_artifactlist_xls_artifact_note_internal_form_label(
-        self,
-    ):
-        """test form label"""
-
-        # get object
-        form = ArtifactExporterSpreadsheetXlsConfigForm()
-        # compare
         self.assertEqual(
-            form.fields['artifactlist_xls_artifact_note_internal'].label,
-            'Export internal note',
+            form.fields['artifactlist_xls_system_id'].label, 'Export system ID'
         )
-
-    def test_artifact_exporter_spreadsheet_xls_config_artifactlist_xls_artifact_note_external_form_label(
-        self,
-    ):
-        """test form label"""
-
-        # get object
-        form = ArtifactExporterSpreadsheetXlsConfigForm()
-        # compare
         self.assertEqual(
-            form.fields['artifactlist_xls_artifact_note_external'].label,
-            'Export external note',
+            form.fields['artifactlist_xls_system_name'].label, 'Export system name'
         )
-
-    def test_artifact_exporter_spreadsheet_xls_config_artifactlist_xls_artifact_note_analysisresult_form_label(
-        self,
-    ):
-        """test form label"""
-
-        # get object
-        form = ArtifactExporterSpreadsheetXlsConfigForm()
-        # compare
+        self.assertEqual(
+            form.fields['artifactlist_xls_tag_all'].label, 'Export tags'
+        )
         self.assertEqual(
             form.fields['artifactlist_xls_artifact_note_analysisresult'].label,
             'Export analysis result',
         )
-
-    def test_artifact_exporter_spreadsheet_xls_config_artifactlist_xls_artifact_md5_form_label(
-        self,
-    ):
-        """test form label"""
-
-        # get object
-        form = ArtifactExporterSpreadsheetXlsConfigForm()
-        # compare
+        self.assertEqual(
+            form.fields['artifactlist_xls_artifact_note_external'].label,
+            'Export external note',
+        )
+        self.assertEqual(
+            form.fields['artifactlist_xls_artifact_note_internal'].label,
+            'Export internal note',
+        )
         self.assertEqual(
             form.fields['artifactlist_xls_artifact_md5'].label, 'Export MD5'
         )
-
-    def test_artifact_exporter_spreadsheet_xls_config_artifactlist_xls_artifact_sha1_form_label(
-        self,
-    ):
-        """test form label"""
-
-        # get object
-        form = ArtifactExporterSpreadsheetXlsConfigForm()
-        # compare
         self.assertEqual(
             form.fields['artifactlist_xls_artifact_sha1'].label, 'Export SHA1'
         )
-
-    def test_artifact_exporter_spreadsheet_xls_config_artifactlist_xls_artifact_sha256_form_label(
-        self,
-    ):
-        """test form label"""
-
-        # get object
-        form = ArtifactExporterSpreadsheetXlsConfigForm()
-        # compare
         self.assertEqual(
             form.fields['artifactlist_xls_artifact_sha256'].label, 'Export SHA256'
         )
-
-    def test_artifact_exporter_spreadsheet_xls_config_artifactlist_xls_artifact_create_time_form_label(
-        self,
-    ):
-        """test form label"""
-
-        # get object
-        form = ArtifactExporterSpreadsheetXlsConfigForm()
-        # compare
+        self.assertEqual(
+            form.fields['artifactlist_xls_artifact_source_path'].label,
+            'Export source path',
+        )
+        self.assertEqual(
+            form.fields['artifactlist_xls_artifact_storage_path'].label,
+            'Export storage path',
+        )
+        self.assertEqual(
+            form.fields['artifactlist_xls_artifact_assigned_to_user_id'].label,
+            'Export assigned_to user',
+        )
         self.assertEqual(
             form.fields['artifactlist_xls_artifact_create_time'].label,
             'Export create time',
         )
-
-    def test_artifact_exporter_spreadsheet_xls_config_artifactlist_xls_artifact_modify_time_form_label(
-        self,
-    ):
-        """test form label"""
-
-        # get object
-        form = ArtifactExporterSpreadsheetXlsConfigForm()
-        # compare
+        self.assertEqual(
+            form.fields['artifactlist_xls_artifact_created_by_user_id'].label,
+            'Export created by user',
+        )
         self.assertEqual(
             form.fields['artifactlist_xls_artifact_modify_time'].label,
             'Export modify time',
         )
-
-    def test_artifact_exporter_spreadsheet_xls_config_artifactlist_xls_worksheet_artifactstatus_form_label(
-        self,
-    ):
-        """test form label"""
-
-        # get object
-        form = ArtifactExporterSpreadsheetXlsConfigForm()
-        # compare
+        self.assertEqual(
+            form.fields['artifactlist_xls_artifact_modified_by_user_id'].label,
+            'Export modified by user',
+        )
         self.assertEqual(
             form.fields['artifactlist_xls_worksheet_artifactstatus'].label,
             'Export worksheet to explain artifactstatus',
         )
-
-    def test_artifact_exporter_spreadsheet_xls_config_artifactlist_xls_worksheet_artifacttype_form_label(
-        self,
-    ):
-        """test form label"""
-
-        # get object
-        form = ArtifactExporterSpreadsheetXlsConfigForm()
-        # compare
         self.assertEqual(
             form.fields['artifactlist_xls_worksheet_artifacttype'].label,
             'Export worksheet to explain artifacttype',
