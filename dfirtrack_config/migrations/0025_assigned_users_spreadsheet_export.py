5c5b9ba04b1c35efdf984a3f75d952311c0944e6
Mathias Stuhlmacher <mathias.stuhlmacher@gmx.de>
2022-02-22 22:34:39 +0100

    simplified migrations

--- dfirtrack_config/migrations/0025_assigned_users_spreadsheet_export.py
+++ dfirtrack_config/migrations/0025_assigned_users_spreadsheet_export.py
@@ -38,6 +38,36 @@ class Migration(migrations.Migration):
             name='artifactlist_xls_tag_all',
             field=models.BooleanField(default=False),
         ),
+        migrations.AddField(
+            model_name='systemexporterspreadsheetcsvconfigmodel',
+            name='spread_csv_system_assigned_to_user_id',
+            field=models.BooleanField(default=False),
+        ),
+        migrations.AddField(
+            model_name='systemexporterspreadsheetcsvconfigmodel',
+            name='spread_csv_system_created_by_user_id',
+            field=models.BooleanField(default=False),
+        ),
+        migrations.AddField(
+            model_name='systemexporterspreadsheetcsvconfigmodel',
+            name='spread_csv_system_modified_by_user_id',
+            field=models.BooleanField(default=False),
+        ),
+        migrations.AddField(
+            model_name='systemexporterspreadsheetxlsconfigmodel',
+            name='spread_xls_system_assigned_to_user_id',
+            field=models.BooleanField(default=False),
+        ),
+        migrations.AddField(
+            model_name='systemexporterspreadsheetxlsconfigmodel',
+            name='spread_xls_system_created_by_user_id',
+            field=models.BooleanField(default=False),
+        ),
+        migrations.AddField(
+            model_name='systemexporterspreadsheetxlsconfigmodel',
+            name='spread_xls_system_modified_by_user_id',
+            field=models.BooleanField(default=False),
+        ),
         migrations.AlterField(
             model_name='artifactexporterspreadsheetxlsconfigmodel',
             name='artifactlist_xls_artifactpriority',
