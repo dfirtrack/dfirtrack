20ab3cbc1174dce2c8078cbbb55db2bd51dacd14
n3x <spueschel89@gmail.com>
2022-03-13 21:38:40 +0100

    Initial creation of tests for api completness

---
+++ dfirtrack_api/tests/utils/test_utils_api_completness.py
@@ -0,0 +1,22 @@
+import yaml
+from django.test import TestCase
+
+class DFIRTrackUtilityAPICompletnessTestCase(TestCase):
+    """
+    Test case for the utlity that helps to determine
+    the completness of the API
+    """
+
+    @classmethod
+    def setUpTestData(cls):
+        # load openapi specification
+        pass
+
+    def test_load_openapi_specification(self):
+        with open('dfirtrack_api/openapi/openapi_dfirtrack.yml') as spec:
+            try:
+                openapiSpec=yaml.safe_load(spec)
+            except yaml.YAMLError as e:
+                print(e)
+            
+            self.assertIsNotNone(openapiSpec)
