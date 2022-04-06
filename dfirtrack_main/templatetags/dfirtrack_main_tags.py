19d6d1cfc93c7caa5c86bc8924eb656b0b3b127c
Thomas Kropeit <thomas@kropeit.de>
2022-04-06 15:18:18 +0200

    use variables to reference static dependencies

--- dfirtrack_main/templatetags/dfirtrack_main_tags.py
+++ dfirtrack_main/templatetags/dfirtrack_main_tags.py
@@ -7,3 +7,23 @@ register = template.Library()
 def dfirtrack_version():
     versionnumber = 'v2.4.6'
     return versionnumber
+
+@register.simple_tag
+def dep_bootstrap_version():
+    return 'bootstrap-5.1.3'
+
+@register.simple_tag
+def dep_clipboard_version():
+    return 'clipboard-2.0.10'
+
+@register.simple_tag
+def dep_datatables_version():
+    return 'datatables-1.11.5'
+
+@register.simple_tag
+def dep_jquery_version():
+    return 'jquery-3.6.0'
+
+@register.simple_tag
+def dep_swagger_ui_version():
+    return 'swagger-ui-4.9.0'
