3a039900f4b2e14ab76f633b7645e24d288ede31
Mathias Stuhlmacher <mathias.stuhlmacher@gmx.de>
2022-04-06 11:51:34 +0200

    moved optional imports to fit to local settings files

--- dfirtrack/settings.py
+++ dfirtrack/settings.py
@@ -199,6 +199,15 @@ use settings from this file in case of missing local settings
 try statements are split to avoid conflicts in case of missing values
 """
 
+# DEBUG
+try:
+    from .local_settings import DEBUG
+
+except ImportError:  # coverage: ignore branch
+
+    # change to True for debugging
+    DEBUG = False
+
 # SESSION_COOKIE_SECURE
 try:
     from .local_settings import SESSION_COOKIE_SECURE
@@ -244,23 +253,6 @@ except ImportError:  # coverage: ignore branch
             }
         }
 
-# DATA_UPLOAD_MAX_NUMBER_FIELDS
-try:
-    from .local_settings import DATA_UPLOAD_MAX_NUMBER_FIELDS
-
-except ImportError:  # coverage: ignore branch
-
-    DATA_UPLOAD_MAX_NUMBER_FIELDS = 10000
-
-# DEBUG
-try:
-    from .local_settings import DEBUG
-
-except ImportError:  # coverage: ignore branch
-
-    # change to True for debugging
-    DEBUG = False
-
 # STATIC_ROOT
 try:
     from .local_settings import STATIC_ROOT
@@ -268,3 +260,11 @@ try:
 except ImportError:  # coverage: ignore branch
 
     STATIC_ROOT = '/var/www/html/static/'
+
+# DATA_UPLOAD_MAX_NUMBER_FIELDS
+try:
+    from .local_settings import DATA_UPLOAD_MAX_NUMBER_FIELDS
+
+except ImportError:  # coverage: ignore branch
+
+    DATA_UPLOAD_MAX_NUMBER_FIELDS = 10000
