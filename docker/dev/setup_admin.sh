1b0ba73da0bb2b9d2521a779f2657ab8660d4c91
FabFaeb <fabfaeb@gmail.com>
2026-01-08 09:30:40 +0100

    update docker containers for oidc

--- docker/dev/setup_admin.sh
+++ docker/dev/setup_admin.sh
@@ -5,5 +5,5 @@ read superuser_name
 echo "Enter Superuser email:"
 read superuser_email
 echo ""
-docker container exec -it dev_dfirtrack_1 /dfirtrack/manage.py createsuperuser --noinput --username $superuser_name --email $superuser_email
-docker container exec -it dev_dfirtrack_1 /dfirtrack/manage.py changepassword $superuser_name
+docker container exec -it dev-dfirtrack-1 /dfirtrack/manage.py createsuperuser --noinput --username $superuser_name --email $superuser_email
+docker container exec -it dev-dfirtrack-1 /dfirtrack/manage.py changepassword $superuser_name
